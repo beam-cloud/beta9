31709cbb36c69e660140784d8f1d6bbb8796b1c7
Luke Lombardi <luke@beam.cloud>
2023-12-06 21:59:24 -0500

    remove imageservice rpc

--- proto/gen.sh
+++ proto/gen.sh
@@ -2,7 +2,7 @@
 
 # Generate Go code
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=./ --go-grpc_opt=paths=source_relative ./scheduler.proto
-protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=./ --go-grpc_opt=paths=source_relative ./imageservice.proto
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=./ --go-grpc_opt=paths=source_relative ./gateway.proto
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=./ --go-grpc_opt=paths=source_relative ./cache.proto
 
 # Generate code for abstractions
