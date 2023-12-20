1d775b74acd6a47d8953a088e0b0147891d235f4
Luke Lombardi <luke@beam.cloud>
2023-12-20 12:24:18 -0500

    finish sync endpoints

--- internal/gateway/service.go
+++ internal/gateway/service.go
@@ -2,7 +2,10 @@ package gateway
 
 import (
 	"context"
-	"log"
+	"crypto/sha256"
+	"encoding/hex"
+	"os"
+	"path"
 
 	pb "github.com/beam-cloud/beam/proto"
 )
@@ -13,23 +16,69 @@ type GatewayService struct {
 }
 
 func NewGatewayService(gw *Gateway) (*GatewayService, error) {
+	os.MkdirAll(GatewayConfig.DefaultObjectPath, 0644)
+
 	return &GatewayService{
 		gw: gw,
 	}, nil
 }
 
 func (gws *GatewayService) HeadObject(ctx context.Context, in *pb.HeadObjectRequest) (*pb.HeadObjectResponse, error) {
-	// TODO: implement HeadObject
+	filePath := path.Join(GatewayConfig.DefaultObjectPath, in.ObjectId)
+
+	fileInfo, err := os.Stat(filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return &pb.HeadObjectResponse{
+				Ok:       true,
+				Exists:   false,
+				ErrorMsg: "Object not found.",
+			}, nil
+		}
+
+		return &pb.HeadObjectResponse{
+			Ok:       true,
+			Exists:   false,
+			ErrorMsg: err.Error(),
+		}, nil
+	}
+
+	metadata := &pb.ObjectMetadata{
+		Name: fileInfo.Name(),
+		Size: fileInfo.Size(),
+	}
+
 	return &pb.HeadObjectResponse{
-		Ok: true,
+		Ok:             true,
+		Exists:         true,
+		ObjectMetadata: metadata,
 	}, nil
 }
 
 func (gws *GatewayService) PutObject(ctx context.Context, in *pb.PutObjectRequest) (*pb.PutObjectResponse, error) {
-	log.Println("metadata:", in.ObjectMetadata)
+	hash := sha256.Sum256(in.ObjectContent)
+	objectId := hex.EncodeToString(hash[:])
+	filePath := path.Join(GatewayConfig.DefaultObjectPath, objectId)
 
-	// TODO: implement PutObject
+	// Check if object already exists
+	_, err := os.Stat(filePath)
+	if err == nil && !in.Overwrite {
+		return &pb.PutObjectResponse{
+			Ok:       true,
+			ObjectId: objectId,
+		}, nil
+	}
+
+	err = os.WriteFile(filePath, in.ObjectContent, 0644)
+	if err != nil {
+		return &pb.PutObjectResponse{
+			Ok:       false,
+			ErrorMsg: err.Error(),
+		}, nil
+	}
+
 	return &pb.PutObjectResponse{
-		Ok: true,
+		Ok:       true,
+		ObjectId: objectId,
 	}, nil
 }
