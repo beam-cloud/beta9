98432d2fb9898bbbcd8d24faf34ade0222c1e516
luke-lombardi <33990301+luke-lombardi@users.noreply.github.com>
2023-12-28 18:46:42 -0500

    GRPC Authentication (#20)
    * add configure endpoint
    
    * working on auth
    
    * add docs placeholder
    
    * create authenticated channel class
    
    * got a token
    
    * handle streams too
    
    * add authinterceptor struct
    
    * wip
    
    * add auth package
    
    * wip
    
    * cleaning things up
    
    * locking shit down
    
    * rename config file
    
    * wip
    
    * create new tokens
    
    * update msg
    
    * create token
    
    * pass tokens
    
    * remove build files
    
    * remove egg
    
    ---------
    
    Co-authored-by: Luke Lombardi <luke@beam.cloud>

--- sdk/src/beam/config.py
+++ sdk/src/beam/config.py
@@ -1,33 +1,142 @@
+import configparser
 import functools
 import os
 import sys
 from contextlib import contextmanager
-from typing import Any, Callable, NamedTuple
+from typing import Any, Callable, NamedTuple, Optional, Type, Union, cast
 
-from grpclib.client import Channel
+from grpclib.client import Channel, Stream, _MetadataLike
+from grpclib.const import Cardinality
+from grpclib.metadata import Deadline
+from multidict import MultiDict
+
+from beam import terminal
+from beam.aio import run_sync
+from beam.clients.gateway import AuthorizeResponse, GatewayServiceStub
+
+DEFAULT_CONFIG_FILE_PATH = "~/.beam/creds"
+DEFAULT_PROFILE_NAME = "default"
+DEFAULT_GATEWAY_HOST = "0.0.0.0"
+DEFAULT_GATEWAY_PORT = "1993"
 
 
 class GatewayConfig(NamedTuple):
-    host: str = "0.0.0.0"
-    port: int = 1993
+    gateway_url: str = DEFAULT_GATEWAY_HOST
+    gateway_port: str = DEFAULT_GATEWAY_PORT
+    token: Optional[str] = None
+
+
+class AuthenticatedChannel(Channel):
+    def __init__(self, *args, token: Optional[str] = None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._token = token
+
+    def request(
+        self,
+        name: str,
+        cardinality: Cardinality,
+        request_type: Type,
+        reply_type: Type,
+        *,
+        timeout: Optional[float] = None,
+        deadline: Optional[Deadline] = None,
+        metadata: Optional[_MetadataLike] = None,
+    ) -> Stream:
+        if self._token:
+            metadata = cast(MultiDict, MultiDict(metadata or ()))
+            metadata["authorization"] = f"Bearer {self._token}"
+
+        return super().request(
+            name,
+            cardinality,
+            request_type,
+            reply_type,
+            timeout=timeout,
+            deadline=deadline,
+            metadata=metadata,
+        )
+
+
+def load_config_from_file() -> GatewayConfig:
+    config_path = os.path.expanduser(DEFAULT_CONFIG_FILE_PATH)
+    config = configparser.ConfigParser()
+
+    if not os.path.exists(config_path):
+        return GatewayConfig()
+
+    config.read(config_path)
+
+    gateway_url = config.get(DEFAULT_PROFILE_NAME, "gateway_url", fallback=DEFAULT_GATEWAY_HOST)
+    gateway_port = config.get(DEFAULT_PROFILE_NAME, "gateway_port", fallback=DEFAULT_GATEWAY_PORT)
+    token = config.get(DEFAULT_PROFILE_NAME, "token", fallback=None)
+
+    return GatewayConfig(gateway_url, gateway_port, token)
+
+
+def save_config_to_file(config: GatewayConfig) -> None:
+    config_path = os.path.expanduser(DEFAULT_CONFIG_FILE_PATH)
+    os.makedirs(os.path.dirname(config_path), exist_ok=True)
+
+    config_parser = configparser.ConfigParser()
+    config_parser[DEFAULT_PROFILE_NAME] = config._asdict()
+
+    with open(config_path, "w") as file:
+        config_parser.write(file)
 
 
 def get_gateway_config() -> GatewayConfig:
-    host = os.getenv("BEAM_GATEWAY_HOST", "0.0.0.0")
-    port = os.getenv("BEAM_GATEWAY_PORT", 1993)
+    gateway_url = os.getenv("BEAM_GATEWAY_HOST", None)
+    gateway_port = os.getenv("BEAM_GATEWAY_PORT", None)
+    token = os.getenv("BEAM_TOKEN", None)
 
-    # TODO: add token to this
-    config = GatewayConfig(host=host, port=port)
-    return config
+    if gateway_url and gateway_port and token:
+        return GatewayConfig(gateway_url, gateway_port, token)
+
+    return load_config_from_file()
 
 
 def get_gateway_channel() -> Channel:
     config: GatewayConfig = get_gateway_config()
-    return Channel(
-        host=config.host,
-        port=config.port,
-        ssl=True if config.port == 443 else False,
+    channel: Union[AuthenticatedChannel, None] = None
+
+    if config.token is None:
+        channel = AuthenticatedChannel(
+            host=config.gateway_url,
+            port=int(config.gateway_port),
+            ssl=True if config.gateway_port == "443" else False,
+            token=None,
+        )
+
+        terminal.header("Welcome to Beam! Let's get started 📡")
+
+        gateway_url = terminal.prompt(text="Gateway host", default="0.0.0.0")
+        gateway_port = terminal.prompt(text="Gateway port", default="1993")
+        terminal.prompt(text="Token", default=None)
+
+        terminal.header("Authorizing with gateway")
+        gateway_stub = GatewayServiceStub(channel=channel)
+        auth_response: AuthorizeResponse = run_sync(gateway_stub.authorize())
+        if not auth_response.ok:
+            channel.close()
+            terminal.error(f"Unable to authorize with gateway: {auth_response.error_msg} ☠️")
+
+        config = config._replace(
+            gateway_url=gateway_url, gateway_port=gateway_port, token=auth_response.new_token
+        )
+
+        save_config_to_file(config)
+
+        channel.close()  # Close unauthenticated channel
+
+    # Open new channel with valid token
+    channel = AuthenticatedChannel(
+        host=config.gateway_url,
+        port=int(config.gateway_port),
+        ssl=True if config.gateway_port == "443" else False,
+        token=config.token,
     )
+
+    return channel
 
 
 @contextmanager
--- sdk/src/beam/terminal.py
+++ sdk/src/beam/terminal.py
@@ -1,3 +1,4 @@
+import sys
 from contextlib import contextmanager
 
 from rich.console import Console
@@ -6,29 +7,37 @@ from rich.text import Text
 _console = Console()
 
 
-def header(text):
+def header(text: str) -> None:
     header_text = f"=> {text}"
     _console.print(Text(header_text, style="bold white"))
 
 
-def detail(text, dim=True):
+def prompt(*, text: str, default: str) -> str:
+    prompt_text = f"=> {text} [{default}]: "
+    _console.print(Text(prompt_text, style="bold blue"), end="")
+    user_input = input().strip()
+    return user_input if user_input else default
+
+
+def detail(text: str, dim: bool = True) -> None:
     style = "dim" if dim else ""
     _console.print(Text(text, style=style))
 
 
-def success(text):
+def success(text: str) -> None:
     _console.print(Text(text, style="bold green"))
 
 
-def error(text):
+def error(text: str) -> None:
     _console.print(Text(text, style="bold red"))
+    sys.exit(1)
 
 
-def url(text):
+def url(text: str) -> None:
     _console.print(Text(text, style="underline blue"))
 
 
 @contextmanager
-def progress(task_name):
+def progress(task_name: str):
     with _console.status(task_name, spinner="dots", spinner_style="white"):
         yield
