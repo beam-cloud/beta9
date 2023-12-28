acbab0cb86acc6e3dbfa62bfc771c2f5b2fe6e0a
Luke Lombardi <luke@beam.cloud>
2023-12-28 10:12:14 -0500

    add docs placeholder

--- sdk/src/beam/config.py
+++ sdk/src/beam/config.py
@@ -8,9 +8,11 @@ from typing import Any, Callable, NamedTuple, Optional
 from grpclib.client import Channel
 
 from beam import terminal
+from beam.aio import run_sync
+from beam.clients.gateway import GatewayServiceStub
 
 DEFAULT_CONFIG_FILE_PATH = "~/.beam/.config"
-DEFAULT_CONTEXT_NAME = "default"
+DEFAULT_PROFILE_NAME = "default"
 
 
 class GatewayConfig(NamedTuple):
@@ -27,9 +29,9 @@ def load_config_from_file() -> GatewayConfig:
         return GatewayConfig()
 
     config.read(config_path)
-    gateway_url = config.get(DEFAULT_CONTEXT_NAME, "gateway_url", fallback="0.0.0.0")
-    gateway_port = config.get(DEFAULT_CONTEXT_NAME, "gateway_port", fallback="1993")
-    token = config.get(DEFAULT_CONTEXT_NAME, "token", fallback=None)
+    gateway_url = config.get(DEFAULT_PROFILE_NAME, "gateway_url", fallback="0.0.0.0")
+    gateway_port = config.get(DEFAULT_PROFILE_NAME, "gateway_port", fallback="1993")
+    token = config.get(DEFAULT_PROFILE_NAME, "token", fallback=None)
 
     return GatewayConfig(gateway_url, gateway_port, token)
 
@@ -39,7 +41,7 @@ def save_config_to_file(config: GatewayConfig) -> None:
     os.makedirs(os.path.dirname(config_path), exist_ok=True)
 
     config_parser = configparser.ConfigParser()
-    config_parser[DEFAULT_CONTEXT_NAME] = config._asdict()
+    config_parser[DEFAULT_PROFILE_NAME] = config._asdict()
 
     with open(config_path, "w") as file:
         config_parser.write(file)
@@ -49,26 +51,30 @@ def get_gateway_config() -> GatewayConfig:
     config = load_config_from_file()
 
     if config.token is None:
-        terminal.prompt("Welcome! Enter your token: ")
-
-        token = input()
-
-        if token.strip() == "":
-            terminal.error("Invalid token.")
-
-        config = config._replace(token=token)
-        save_config_to_file(config)
+        terminal.header("Welcome to Beam 📡 ")
+        gateway_url = terminal.prompt(text="Gateway host", default="0.0.0.0")
+        gateway_port = terminal.prompt(text="Gateway port", default="1993")
+        config = config._replace(gateway_url=gateway_url, gateway_port=gateway_port)
+        # save_config_to_file(config)
 
     return config
 
 
 def get_gateway_channel() -> Channel:
     config: GatewayConfig = get_gateway_config()
-    return Channel(
+    channel = Channel(
         host=config.gateway_url,
         port=int(config.gateway_port),
         ssl=True if config.gateway_port == "443" else False,
     )
+
+    if config.token is None:
+        terminal.header("Configuring gateway")
+        stub = GatewayServiceStub(channel=channel)
+        run_sync(stub.configure(name="test-thing", token=""))
+
+    setattr(channel, "token", config.token)
+    return channel
 
 
 @contextmanager
