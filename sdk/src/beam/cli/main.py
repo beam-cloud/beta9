27e6992bbba0bfd289110f7654bb61efc2c7cf37
Nick Petrovic <4001122+nickpetrovic@users.noreply.github.com>
2024-01-08 15:37:38 -0500

    SDK: Add filter option to beam tasks list command (#29)

--- sdk/src/beam/cli/main.py
+++ sdk/src/beam/cli/main.py
@@ -1,6 +1,10 @@
+import shutil
+
 import click
 
 from beam.cli import configure, tasks
+
+click.formatting.FORCED_WIDTH = shutil.get_terminal_size().columns
 
 cli = click.Group()
 cli.add_command(configure.configure)
