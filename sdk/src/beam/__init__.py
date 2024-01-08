cabba55d664d219f1bbae97294694a0615fe2b8d
Luke Lombardi <luke@beam.cloud>
2024-01-08 17:12:07 -0500

    try to fix merge confllicts

--- sdk/src/beam/__init__.py
+++ sdk/src/beam/__init__.py
@@ -2,14 +2,8 @@ from .abstractions.function import Function
 from .abstractions.image import Image
 from .abstractions.map import Map
 from .abstractions.queue import SimpleQueue as Queue
-<<<<<<< HEAD
 from .abstractions.taskqueue import TaskQueue
-
-__version__ = "0.0.1"
-__all__ = ["__version__", "Function", "Map", "Image", "Queue", "TaskQueue"]
-=======
 from .abstractions.volume import Volume
 
 __version__ = "0.0.1"
-__all__ = ["__version__", "Function", "Map", "Image", "Queue", "Volume"]
->>>>>>> master
+__all__ = ["__version__", "Function", "Map", "Image", "Queue", "TaskQueue", "Volume"]
--- sdk/src/beam/abstractions/function.py
+++ sdk/src/beam/abstractions/function.py
@@ -1,18 +1,12 @@
 import asyncio
 import os
-from typing import Any, Callable, Iterable, List, Optional, Union
+from typing import Any, Callable, Iterable, Union
 
 import cloudpickle
 
 from beam import terminal
-<<<<<<< HEAD
 from beam.abstractions.image import Image
 from beam.abstractions.runner import RunnerAbstraction
-=======
-from beam.abstractions.base import BaseAbstraction
-from beam.abstractions.image import Image, ImageBuildResult
-from beam.abstractions.volume import Volume
->>>>>>> master
 from beam.clients.function import (
     FunctionInvokeResponse,
     FunctionServiceStub,
@@ -23,42 +17,10 @@ FUNCTION_STUB_TYPE = "FUNCTION"
 FUNCTION_STUB_PREFIX = "function"
 
 
-<<<<<<< HEAD
 class Function(RunnerAbstraction):
     def __init__(self, image: Image, cpu: int = 100, memory: int = 128, gpu="") -> None:
         super().__init__(image=image, cpu=cpu, memory=memory, gpu=gpu)
 
-=======
-class Function(BaseAbstraction):
-    def __init__(
-        self,
-        image: Image,
-        cpu: int = 100,
-        memory: int = 128,
-        gpu="",
-        volumes: Optional[List[Volume]] = None,
-    ) -> None:
-        super().__init__()
-
-        if image is None:
-            image = Image()
-
-        self.image: Image = image
-        self.image_available: bool = False
-        self.files_synced: bool = False
-        self.stub_created: bool = False
-        self.runtime_ready: bool = False
-        self.object_id: str = ""
-        self.image_id: str = ""
-        self.stub_id: str = ""
-        self.handler: str = ""
-        self.cpu = cpu
-        self.memory = memory
-        self.gpu = gpu
-        self.volumes = volumes or []
-
-        self.gateway_stub: GatewayServiceStub = GatewayServiceStub(self.channel)
->>>>>>> master
         self.function_stub: FunctionServiceStub = FunctionServiceStub(self.channel)
         self.syncer: FileSyncer = FileSyncer(self.gateway_stub)
 
@@ -71,66 +33,6 @@ class _CallableWrapper:
         self.func: Callable = func
         self.parent: Function = parent
 
-<<<<<<< HEAD
-=======
-    def _prepare_runtime(self) -> bool:
-        module = inspect.getmodule(self.func)  # Determine module / function name
-        if module:
-            module_file = os.path.basename(module.__file__)
-            module_name = os.path.splitext(module_file)[0]
-        else:
-            module_name = "__main__"
-
-        function_name = self.func.__name__
-        self.parent.handler = f"{module_name}:{function_name}"
-
-        if not self.parent.image_available:
-            image_build_result: ImageBuildResult = self.parent.image.build()
-
-            if image_build_result and image_build_result.success:
-                self.parent.image_available = True
-                self.parent.image_id = image_build_result.image_id
-            else:
-                return False
-
-        if not self.parent.files_synced:
-            sync_result = self.parent.syncer.sync()
-
-            if sync_result.success:
-                self.parent.files_synced = True
-                self.parent.object_id = sync_result.object_id
-            else:
-                return False
-
-        for volume in self.parent.volumes:
-            if not volume.ready and not volume.get_or_create():
-                return False
-
-        if not self.parent.stub_created:
-            stub_response: GetOrCreateStubResponse = self.parent.run_sync(
-                self.parent.gateway_stub.get_or_create_stub(
-                    object_id=self.parent.object_id,
-                    image_id=self.parent.image_id,
-                    stub_type=FUNCTION_STUB_TYPE,
-                    name=f"{FUNCTION_STUB_PREFIX}/{self.parent.handler}",
-                    python_version=self.parent.image.python_version,
-                    cpu=self.parent.cpu,
-                    memory=self.parent.memory,
-                    gpu=self.parent.gpu,
-                    volumes=[volume.export() for volume in self.parent.volumes],
-                )
-            )
-
-            if stub_response.ok:
-                self.parent.stub_created = True
-                self.parent.stub_id = stub_response.stub_id
-            else:
-                return False
-
-        self.parent.runtime_ready = True
-        return True
-
->>>>>>> master
     def __call__(self, *args, **kwargs) -> Any:
         container_id = os.getenv("CONTAINER_ID")
         if container_id:
@@ -161,15 +63,6 @@ class _CallableWrapper:
         async for r in self.parent.function_stub.function_invoke(
             stub_id=self.parent.stub_id,
             args=args,
-<<<<<<< HEAD
-=======
-            handler=self.parent.handler,
-            python_version=self.parent.image.python_version,
-            cpu=self.parent.cpu,
-            memory=self.parent.memory,
-            gpu=self.parent.gpu,
-            volumes=[volume.export() for volume in self.parent.volumes],
->>>>>>> master
         ):
             if r.output != "":
                 terminal.detail(r.output)
