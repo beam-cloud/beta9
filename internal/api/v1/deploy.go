d9f27c485bf44b1536803115f9c302d27c76f5d9
Luke Lombardi <luke@beam.cloud>
2024-01-09 13:42:47 -0500

    updated pr to show usage

--- internal/api/v1/deploy.go
+++ internal/api/v1/deploy.go
@@ -1,8 +1,10 @@
 package apiv1
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/beam-cloud/beam/internal/auth"
 	"github.com/beam-cloud/beam/internal/common"
 	"github.com/labstack/echo/v4"
 )
@@ -19,6 +21,11 @@ func NewDeployGroup(g *echo.Group, rdb *common.RedisClient) *HealthGroup {
 }
 
 func (h *HealthGroup) ListDeploys(ctx echo.Context) error {
+	cc, _ := ctx.(*auth.HttpAuthContext)
+
+	log.Println("Token value: ", cc.AuthInfo.Token.ExternalId)
+	log.Println("Workspace name: ", cc.AuthInfo.Workspace.Name)
+
 	return ctx.JSON(http.StatusOK, map[string]interface{}{
 		"status": "ok",
 	})
