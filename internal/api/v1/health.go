ddc673df58c1013e6bbbae0ddb259a02a087ec6e
Luke Lombardi <luke@beam.cloud>
2024-01-09 12:07:02 -0500

    fix comment

--- internal/api/v1/health.go
+++ internal/api/v1/health.go
@@ -26,9 +26,8 @@ func NewHealthGroup(g *echo.Group, rdb *common.RedisClient) *HealthGroup {
 func (h *HealthGroup) HealthCheck(ctx echo.Context) error {
 	err := h.redisClient.Ping(context.Background()).Err()
 
-	log.Printf("health check failed: %v\n", err)
-
 	if err != nil {
+		log.Printf("health check failed: %v\n", err)
 		return ctx.JSON(http.StatusInternalServerError, map[string]interface{}{
 			"status": "not ok",
 			"error":  err.Error(),
