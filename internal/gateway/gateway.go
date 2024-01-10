40456050a52ef991d689cfb931f7e95a47725a34
Luke Lombardi <luke@beam.cloud>
2024-01-10 15:47:18 -0500

    hide echo banner

--- internal/gateway/gateway.go
+++ internal/gateway/gateway.go
@@ -94,6 +94,7 @@ func NewGateway() (*Gateway, error) {
 
 func (g *Gateway) initHttp() error {
 	g.httpServer = echo.New()
+	g.httpServer.HideBanner = true
 
 	g.httpServer.Use(middleware.Logger())
 	g.httpServer.Use(middleware.Recover())
