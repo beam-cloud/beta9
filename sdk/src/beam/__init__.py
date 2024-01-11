fb44fc72614f19a87469de9a4f75cce44011f39f
Luke Lombardi <luke@beam.cloud>
2024-01-11 18:29:37 -0500

    create factories for function/taskqueue

--- sdk/src/beam/__init__.py
+++ sdk/src/beam/__init__.py
@@ -1,9 +1,9 @@
-from .abstractions.function import Function
+from .abstractions.function import function
 from .abstractions.image import Image
 from .abstractions.map import Map
 from .abstractions.queue import SimpleQueue as Queue
-from .abstractions.taskqueue import TaskQueue
+from .abstractions.taskqueue import task_queue
 from .abstractions.volume import Volume
 
 __version__ = "0.0.1"
-__all__ = ["__version__", "Function", "Map", "Image", "Queue", "TaskQueue", "Volume"]
+__all__ = ["__version__", "Map", "Image", "Queue", "Volume", "task_queue", "function"]
