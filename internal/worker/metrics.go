df43a3db755ba1120a060b302231629a80861648
jsun-m <91754185+jsun-m@users.noreply.github.com>
2024-01-25 13:54:46 -0700

    Add metrics server (#57)
    * Add metrics serve
    
    * remove charts
    
    * cleanup
    
    * remove charts
    
    * setup victoria metrics
    
    * verified
    
    * finished setup
    
    * cleanup
    
    * cleanup
    
    * make async
    
    * cleanup for pr
    
    * cleanup for pr
    
    * clean up comments and changed to TODOs

--- internal/worker/metrics.go
+++ internal/worker/metrics.go
@@ -2,155 +2,55 @@ package worker
 
 import (
 	"context"
+	"log"
 	"time"
 
+	"github.com/beam-cloud/beta9/internal/repository"
 	repo "github.com/beam-cloud/beta9/internal/repository"
 	types "github.com/beam-cloud/beta9/internal/types"
-	"github.com/okteto/okteto/pkg/log"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 type WorkerMetrics struct {
-	workerId          string
-	metricsRepo       repo.MetricsStatsdRepository
-	workerRepo        repo.WorkerRepository
-	metricsStreamRepo repo.MetricsStreamRepository
-	ctx               context.Context
+	workerId    string
+	metricsRepo repo.PrometheusRepository
+	workerRepo  repo.WorkerRepository
+	ctx         context.Context
 }
 
 func NewWorkerMetrics(
 	ctx context.Context,
 	workerId string,
-	metricsRepo repo.MetricsStatsdRepository,
 	workerRepo repo.WorkerRepository,
-	metricsStreamRepo repo.MetricsStreamRepository,
+	config types.PrometheusConfig,
 ) *WorkerMetrics {
-	return &WorkerMetrics{
-		ctx:               ctx,
-		workerId:          workerId,
-		metricsRepo:       metricsRepo,
-		workerRepo:        workerRepo,
-		metricsStreamRepo: metricsStreamRepo,
-	}
-}
-
-func (wm *WorkerMetrics) WorkerStarted() {
-	wm.metricsRepo.WorkerStarted(wm.workerId)
-}
-
-func (wm *WorkerMetrics) WorkerStopped() {
-	wm.metricsRepo.WorkerStopped(wm.workerId)
-}
-
-func (wm *WorkerMetrics) ContainerStarted(containerId string) {
-	wm.metricsRepo.ContainerStarted(
-		containerId,
-		wm.workerId,
+	metricsRepo := repository.NewMetricsPrometheusRepository(config)
+	metricsRepo.RegisterCounterVec(
+		prometheus.CounterOpts{
+			Name: types.MetricsWorkerContainerDurationSeconds,
+		},
+		[]string{"container_id", "worker_id"},
 	)
-}
 
-func (wm *WorkerMetrics) ContainerStopped(containerId string) {
-	wm.metricsRepo.ContainerStopped(
-		containerId,
-		wm.workerId,
-	)
-}
-
-// Periodically send statsd metrics to track worker duration
-func (wm *WorkerMetrics) EmitWorkerDuration() {
-	cursorTime := time.Now()
-	ticker := time.NewTicker(types.WorkerDurationEmissionInterval)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-ticker.C:
-			wm.metricsRepo.WorkerDuration(wm.workerId, time.Now().UnixNano(), time.Since(cursorTime))
-			cursorTime = time.Now()
-		case <-wm.ctx.Done():
-			// Consolidate any remaining time
-			wm.metricsRepo.WorkerDuration(wm.workerId, time.Now().UnixNano(), time.Since(cursorTime))
-			return
+	go func() {
+		if err := metricsRepo.Init(); err != nil {
+			log.Fatalf("Failed to start metrics server: %v", err)
 		}
+	}()
+
+	workerMetrics := &WorkerMetrics{
+		ctx:         ctx,
+		workerId:    workerId,
+		metricsRepo: metricsRepo,
+		workerRepo:  workerRepo,
 	}
+
+	return workerMetrics
 }
 
-func (wm *WorkerMetrics) EmitResourceUsage(request *types.ContainerRequest, pidChan <-chan int, done chan bool) {
-	gpuEnabled := request.Gpu != ""
-
-	ticker := time.NewTicker(types.ContainerResourceUsageEmissionInterval)
-	defer ticker.Stop()
-
-	containerPid := <-pidChan
-	if containerPid == 0 {
-		return
-	}
-
-	containerProc, err := NewProcUtil(containerPid)
-	if err != nil {
-		log.Printf("Unable to get process for container %s: %v\n", request.ContainerId, err)
-		return
-	}
-
-	var prevProcCpuTime float64 = 0
-	var prevSystemCpuTime float64 = 0
-	for {
-		select {
-		case <-ticker.C:
-			var totalCpuTime float64 = 0
-			var totalMem int = 0
-
-			var gpuMemUsed int64 = -1
-			var gpuMemTotal int64 = -1
-
-			if gpuEnabled {
-				stats, err := GetGpuMemoryUsage(0) // TODO: Support multiple GPUs
-				if err == nil {
-					gpuMemUsed = stats.UsedCapacity
-					gpuMemTotal = stats.TotalCapacity
-				}
-			}
-
-			procs, err := containerProc.getAllDescendantProcs()
-			if err != nil {
-				log.Printf("Unable to get descendant PIDs for container %s: %v\n", request.ContainerId, err)
-				continue
-			}
-
-			procs = append(procs, containerProc)
-			for _, p := range procs {
-				stat, err := p.Stat()
-				if err != nil {
-					continue
-				}
-
-				totalCpuTime += stat.CPUTime()
-				totalMem += stat.ResidentMemory()
-			}
-
-			totalSystemCpuTime, err := GetSystemCPU()
-			if err != nil {
-				log.Printf("Unable to get system CPU time for container %s: %v\n", request.ContainerId, err)
-				continue
-			}
-
-			cpuMillicoreUtilization := GetProcCurrentCPUMillicores(totalCpuTime, prevProcCpuTime, totalSystemCpuTime, prevSystemCpuTime)
-
-			wm.metricsStreamRepo.ContainerResourceUsage(
-				types.ContainerResourceUsage{
-					ContainerID:       request.ContainerId,
-					CpuMillicoresUsed: int64(cpuMillicoreUtilization),
-					MemoryUsed:        totalMem,
-					GpuMemoryUsed:     gpuMemUsed,
-					GpuMemoryTotal:    gpuMemTotal,
-					GpuType:           request.Gpu,
-				},
-			)
-
-			prevProcCpuTime = totalCpuTime
-			prevSystemCpuTime = totalSystemCpuTime
-		case <-done:
-			return
-		}
+func (wm *WorkerMetrics) metricsContainerDuration(containerId string, workerId string, duration time.Duration) {
+	if handler := wm.metricsRepo.GetCounterVecHandler(types.MetricsWorkerContainerDurationSeconds); handler != nil {
+		handler.WithLabelValues(containerId, workerId).Add(duration.Seconds())
 	}
 }
 
@@ -163,11 +63,11 @@ func (wm *WorkerMetrics) EmitContainerUsage(request *types.ContainerRequest, don
 	for {
 		select {
 		case <-ticker.C:
-			wm.metricsRepo.ContainerDuration(request.ContainerId, wm.workerId, time.Now().UnixNano(), time.Since(cursorTime))
+			go wm.metricsContainerDuration(request.ContainerId, wm.workerId, time.Since(cursorTime))
 			cursorTime = time.Now()
 		case <-done:
 			// Consolidate any remaining time
-			wm.metricsRepo.ContainerDuration(request.ContainerId, wm.workerId, time.Now().UnixNano(), time.Since(cursorTime))
+			go wm.metricsContainerDuration(request.ContainerId, wm.workerId, time.Since(cursorTime))
 			return
 		}
 	}
