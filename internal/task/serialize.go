43ba2fc3b98a8885d24c4d6856a8f1039b7a5d72
Luke Lombardi <luke@beam.cloud>
2024-04-04 16:35:32 -0400

    fix serializer

--- internal/task/serialize.go
+++ internal/task/serialize.go
@@ -1,10 +1,18 @@
 package task
 
 import (
+	"errors"
+
 	"github.com/beam-cloud/beta9/internal/types"
 	"github.com/labstack/echo/v4"
 )
 
 func SerializeHttpPayload(ctx echo.Context) (*types.TaskPayload, error) {
-	return &types.TaskPayload{}, nil
+	payload := &types.TaskPayload{}
+
+	if err := ctx.Bind(&payload); err != nil {
+		return nil, errors.New("invalid request payload")
+	}
+
+	return payload, nil
 }
