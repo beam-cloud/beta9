c4d546942b79f54722ed48748c556ada041cbcc3
jsun-m <91754185+jsun-m@users.noreply.github.com>
2024-04-05 15:21:34 -0700

    add stub config to api be-1276 (#120)
    * add stub config to api
    
    * remove openmeter api token and fix up stub usage

--- internal/types/filters.go
+++ internal/types/filters.go
@@ -1,8 +1,19 @@
 package types
 
+import "strings"
+
 type BaseFilter struct {
 	Limit  uint32 `query:"limit"`
 	Offset int    `query:"offset"`
+}
+
+// Custom type for a slice of strings
+type StringSlice []string
+
+// UnmarshalParam implements the echo query parameter unmarshaler interface
+func (s *StringSlice) UnmarshalParam(src string) error {
+	*s = strings.Split(src, ",")
+	return nil
 }
 
 type DeploymentFilter struct {
@@ -22,4 +33,9 @@ type TaskFilter struct {
 	CreatedAtStart string `query:"created_at_start"`
 	CreatedAtEnd   string `query:"created_at_end"`
 	Cursor         string `query:"cursor"`
+}
+
+// Struct that includes the custom type
+type StubFilter struct {
+	StubIds StringSlice `query:"stub_ids"` // The query parameter name is "values"
 }
