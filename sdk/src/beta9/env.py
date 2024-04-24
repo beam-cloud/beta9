7f6aa647a11375e3e7b7af9cdd462748e8bfe282
Luke Lombardi <luke@beam.cloud>
2024-04-24 18:15:30 -0400

    cleanup

--- sdk/src/beta9/env.py
+++ sdk/src/beta9/env.py
@@ -1,12 +1,14 @@
 import os
 from functools import wraps
+from typing import Callable
 
 
 def is_local() -> bool:
+    """Check if we are currently in a remote container"""
     return os.getenv("CONTAINER_ID", "") == ""
 
 
-def local_entrypoint(func):
+def local_entrypoint(func: Callable) -> None:
     """Decorator that executes the decorated function if the environment is local (not a remote container)"""
 
     @wraps(func)
