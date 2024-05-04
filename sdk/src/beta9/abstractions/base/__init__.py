05707aa959381ec5dc7fe8fdcf2d7c25487a8c82
Luke Lombardi <luke@beam.cloud>
2024-05-04 10:42:12 -0400

    add serve command and handle connection errors

--- sdk/src/beta9/abstractions/base/__init__.py
+++ sdk/src/beta9/abstractions/base/__init__.py
@@ -6,8 +6,10 @@ from asyncio import AbstractEventLoop
 from pathlib import Path
 from typing import Any, Coroutine, Optional
 
+import grpclib
 from grpclib.client import Channel
 
+from ... import terminal
 from ...channel import get_channel as _get_channel
 from ...config import ConfigContext, SDKSettings, get_config_context, set_settings
 
@@ -65,7 +67,10 @@ class BaseAbstraction(ABC):
         return get_channel()
 
     def run_sync(self, coroutine: Coroutine) -> Any:
-        return self.loop.run_until_complete(coroutine)
+        try:
+            return self.loop.run_until_complete(coroutine)
+        except grpclib.exceptions.StreamTerminatedError:
+            terminal.error("Lost connection to gateway 🔌")
 
     def __init_subclass__(cls, /, **kwargs):
         """
