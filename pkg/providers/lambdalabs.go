786e1b214f8464d0e7ca836de905d1be99cbd40e
luke-lombardi <33990301+luke-lombardi@users.noreply.github.com>
2024-05-15 17:11:03 -0400

    add more scaffold for lambda labs provider

--- pkg/providers/lambdalabs.go
+++ pkg/providers/lambdalabs.go
@@ -2,6 +2,8 @@ package providers
 
 import (
 	"context"
+	"errors"
+	"log"
 
 	"github.com/beam-cloud/beta9/pkg/network"
 	"github.com/beam-cloud/beta9/pkg/repository"
@@ -16,16 +18,39 @@ func NewLambdaLabsProvider(ctx context.Context, appConfig types.AppConfig, provi
 	lambdaLabsProvider := &LambdaLabsProvider{}
 
 	baseProvider := NewExternalProvider(ctx, &ExternalProviderConfig{
-		Name:         string(types.ProviderEC2),
-		ClusterName:  appConfig.ClusterName,
-		AppConfig:    appConfig,
-		TailScale:    tailscale,
-		ProviderRepo: providerRepo,
-		WorkerRepo:   workerRepo,
-		// ListMachinesFunc:     lambdaLabsProvider.listMachines,
-		// TerminateMachineFunc: lambdaLabsProvider.TerminateMachine,
+		Name:                 string(types.ProviderLambdaLabs),
+		ClusterName:          appConfig.ClusterName,
+		AppConfig:            appConfig,
+		TailScale:            tailscale,
+		ProviderRepo:         providerRepo,
+		WorkerRepo:           workerRepo,
+		ListMachinesFunc:     lambdaLabsProvider.listMachines,
+		TerminateMachineFunc: lambdaLabsProvider.TerminateMachine,
 	})
 	lambdaLabsProvider.ExternalProvider = baseProvider
 
 	return lambdaLabsProvider, nil
+}
+
+func (p *LambdaLabsProvider) ProvisionMachine(ctx context.Context, poolName, token string, compute types.ProviderComputeRequest) (string, error) {
+	return "", errors.New("not implemented")
+}
+
+func (p *LambdaLabsProvider) listMachines(ctx context.Context, poolName string) (map[string]string, error) {
+	return map[string]string{}, errors.New("not implemented")
+}
+
+func (p *LambdaLabsProvider) TerminateMachine(ctx context.Context, poolName, instanceId, machineId string) error {
+	if instanceId == "" {
+		return errors.New("invalid instance ID")
+	}
+
+	err := p.ProviderRepo.RemoveMachine(p.Name, poolName, machineId)
+	if err != nil {
+		log.Printf("<provider %s>: Unable to remove machine state <machineId: %s>: %+v\n", p.Name, machineId, err)
+		return err
+	}
+
+	log.Printf("<provider %s>: Terminated machine <machineId: %s> due to inactivity\n", p.Name, machineId)
+	return nil
 }
