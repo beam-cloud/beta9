c7efcfcd009d660b781cc2b7668cd908f8f786e6
John <john@slai.io>
2024-05-23 10:01:11 -0700

    remove log

--- sdk/src/beta9/channel.py
+++ sdk/src/beta9/channel.py
@@ -59,7 +59,6 @@ class AuthenticatedChannel(Channel):
 
 def get_channel(context: Optional[ConfigContext] = None) -> Channel:
     if os.getenv("CI"):
-        print("Using insecure channel for CI")
         return Channel(host="localhost", port=50051, ssl=False)
 
     if not context:
