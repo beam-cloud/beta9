abcc4886286028c18c4be86aecc6ac109a8e250a
luke-lombardi <33990301+luke-lombardi@users.noreply.github.com>
2024-05-23 13:48:18 -0400

    put in mount

--- pkg/storage/juicefs.go
+++ pkg/storage/juicefs.go
@@ -28,6 +28,16 @@ func (s *JuiceFsStorage) Mount(localPath string) error {
 
 	cacheSize := strconv.FormatInt(s.config.CacheSize, 10)
 
+	prefetch := strconv.FormatInt(s.config.Prefetch, 10)
+	if s.config.BlockSize <= 0 {
+		prefetch = "1"
+	}
+
+	bufferSize := strconv.FormatInt(s.config.BufferSize, 10)
+	if s.config.BufferSize <= 0 {
+		prefetch = "300"
+	}
+
 	s.mountCmd = exec.Command(
 		"juicefs",
 		"mount",
@@ -35,8 +45,9 @@ func (s *JuiceFsStorage) Mount(localPath string) error {
 		localPath,
 		"-d",
 		"--bucket", s.config.AWSS3Bucket,
-		"--cache-size",
-		cacheSize,
+		"--cache-size", cacheSize,
+		"--prefetch", prefetch,
+		"--buffer-size", bufferSize,
 		"--no-bgjob",
 		"--no-usage-report",
 	)
@@ -83,24 +94,12 @@ func (s *JuiceFsStorage) Format(fsName string) error {
 		blockSize = "4096"
 	}
 
-	prefetch := strconv.FormatInt(s.config.Prefetch, 10)
-	if s.config.BlockSize <= 0 {
-		prefetch = "1"
-	}
-
-	bufferSize := strconv.FormatInt(s.config.BufferSize, 10)
-	if s.config.BufferSize <= 0 {
-		prefetch = "300"
-	}
-
 	cmd := exec.Command(
 		"juicefs",
 		"format",
 		"--storage", "s3",
 		"--bucket", s.config.AWSS3Bucket,
 		"--block-size", blockSize,
-		"--prefetch", prefetch,
-		"--buffer-size", bufferSize,
 		s.config.RedisURI,
 		fsName,
 		"--no-update",
