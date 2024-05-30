d8d4f3c0b2e00f7fd06905967be50ab2b2da1e12
John <john@slai.io>
2024-05-30 16:38:56 -0700

    update logs

--- bin/verify_proto.sh
+++ bin/verify_proto.sh
@@ -6,9 +6,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-export GO_PATH=~/go
-export PATH=$PATH:/$GO_PATH/bin
-
 tmpWorkDir=$(mktemp -d -t 'twd.XXXXXX')
 mkdir "$tmpWorkDir/beta9"
 tmpWorkDir="$tmpWorkDir/beta9"
@@ -24,7 +21,8 @@ if [ -z "$diff" ]; then
   exit 0
 fi
 echo "Failed genproto-verification!" >&2
-printf "* Found changed files:\n%s\n" "$diff" >&2
+echo "* Found changed files:" "$diff" >&2
+echo $diff >&2
 echo "* Please rerun genproto.sh after changing *.proto file" >&2
-echo "* Run ./scripts/genproto.sh" >&2
+echo "* Run ./scripts/gen_proto.sh" >&2
 exit 1
