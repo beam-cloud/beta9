5ad7659498e732dc7081e28a3a6dbf81a108cf0a
luke-lombardi <33990301+luke-lombardi@users.noreply.github.com>
2024-06-01 11:38:40 -0400

    fix merge conflict

--- pkg/worker/worker.go
+++ pkg/worker/worker.go
@@ -9,8 +9,6 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"runtime"
-	"runtime/pprof"
 	"strconv"
 	"strings"
 	"sync"
@@ -193,39 +191,11 @@ func NewWorker() (*Worker, error) {
 	}, nil
 }
 
-func profileCPU(duration time.Duration) {
-	cpuFile, err := os.Create("cpu_profile.prof")
-	if err != nil {
-		log.Fatalf("Could not create CPU profile: %v", err)
-	}
-	defer cpuFile.Close()
-
-	pprof.StartCPUProfile(cpuFile)
-	time.Sleep(duration)
-	pprof.StopCPUProfile()
-}
-
-func profileMemory(duration time.Duration) {
-	time.Sleep(duration)
-	memFile, err := os.Create("mem_profile.prof")
-	if err != nil {
-		log.Fatalf("Could not create memory profile: %v", err)
-	}
-	defer memFile.Close()
-
-	runtime.GC()
-	pprof.WriteHeapProfile(memFile)
-}
-
 func (s *Worker) Run() error {
-
 	err := s.startup()
 	if err != nil {
 		return err
 	}
-
-	go profileCPU(4 * time.Minute)
-	go profileMemory(4 * time.Minute)
 
 	go s.manageWorkerCapacity()
 	go s.processStopContainerEvents()
