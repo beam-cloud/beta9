61a52d7dd3d275c0be76b78eaee5663eb08923a4
luke-lombardi <33990301+luke-lombardi@users.noreply.github.com>
2024-06-04 18:34:37 -0400

    Listen on TLS in proxy (#283)

--- pkg/api/v1/machine.go
+++ pkg/api/v1/machine.go
@@ -76,7 +76,7 @@ func (g *MachineGroup) RegisterMachine(ctx echo.Context) error {
 		if err != nil {
 			return echo.NewHTTPError(http.StatusInternalServerError, "Unable to lookup service: juicefs-redis")
 		}
-		remoteConfig.Storage.JuiceFS.RedisURI = fmt.Sprintf("redis://%s/0", juiceFsRedisHostname)
+		remoteConfig.Storage.JuiceFS.RedisURI = fmt.Sprintf("rediss://%s/0", juiceFsRedisHostname)
 	}
 
 	gatewayGrpcHostname, err := g.tailscale.GetHostnameForService("gateway-grpc")
