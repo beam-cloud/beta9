1089700ca0e75dd563a2b0a115ae7dca492227df
nickpetrovic <4001122+nickpetrovic@users.noreply.github.com>
2024-06-28 15:42:09 -0400

    Add realtime host to Beam SDK settings (#336)
    Resolve BE-1455

--- sdk/src/beta9/abstractions/base/__init__.py
+++ sdk/src/beta9/abstractions/base/__init__.py
@@ -3,13 +3,14 @@ import inspect
 import os
 from abc import ABC
 from asyncio import AbstractEventLoop
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Any, Coroutine, Optional
 
-from ... import terminal
+from ... import config, terminal
 from ...channel import Channel
 from ...channel import get_channel as _get_channel
-from ...config import ConfigContext, SDKSettings, get_config_context, set_settings
+from ...config import ConfigContext, get_config_context, set_settings
 
 # Global channel
 _channel: Optional[Channel] = None
@@ -82,6 +83,11 @@ class BaseAbstraction(ABC):
         if frame.code_context and any(
             substr in frame.code_context[0] for substr in ("import beam", "from beam")
         ):
+
+            @dataclass
+            class SDKSettings(config.SDKSettings):
+                realtime_host: str = os.getenv("REALTIME_HOST", "wss://rt.beam.cloud")
+
             settings = SDKSettings(
                 name="Beam",
                 api_host=os.getenv("API_HOST", "api.beam.cloud"),
