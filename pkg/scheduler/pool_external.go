29234b5c69522d2ac2e645f7409dd63804e2c245
nickpetrovic <4001122+nickpetrovic@users.noreply.github.com>
2024-07-25 19:16:00 -0400

    Add config for shared memory limit (#401)
    Resolve BE-1578

--- pkg/scheduler/pool_external.go
+++ pkg/scheduler/pool_external.go
@@ -421,7 +421,7 @@ func (wpc *ExternalWorkerPoolController) getWorkerEnvironment(workerId, machineI
 
 func (wpc *ExternalWorkerPoolController) getWorkerVolumes(workerMemory int64) []corev1.Volume {
 	hostPathType := corev1.HostPathDirectoryOrCreate
-	sharedMemoryLimit := resource.MustParse(fmt.Sprintf("%dMi", workerMemory/2))
+	sharedMemoryLimit := calculateMemoryQuantity(wpc.workerPool.PoolSizing.SharedMemoryLimitPct, workerMemory)
 
 	tmpSizeLimit := resource.MustParse("30Gi")
 	return []corev1.Volume{
