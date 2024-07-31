9b92e88d4bf33e5c359f12247a2e2ed676de7edb
John <john@slai.io>
2024-07-31 16:58:31 -0700

    add comma

--- pkg/repository/backend_postgres.go
+++ pkg/repository/backend_postgres.go
@@ -836,7 +836,7 @@ func (c *PostgresBackendRepository) ListLatestDeploymentsWithRelatedPaginated(ct
 	query := squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar).
 		Select(
 			"d.*",
-			"s.external_id AS \"stub.external_id\"", "s.name AS \"stub.name\"", "s.config AS \"stub.config\""
+			"s.external_id AS \"stub.external_id\"", "s.name AS \"stub.name\"", "s.config AS \"stub.config\"",
 		).
 		From("deployment d").
 		Join(`(
