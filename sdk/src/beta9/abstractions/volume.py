609860c3173d63a9742c509ea91d01dcf3380873
Daniel Levi-Minzi <dleviminzi@gmail.com>
2024-08-26 18:12:37 -0400

    update docstring for Volume

--- sdk/src/beta9/abstractions/volume.py
+++ sdk/src/beta9/abstractions/volume.py
@@ -23,10 +23,10 @@ class VolumeConfig:
             The S3 endpoint for the external provider.
     """
 
+    external: bool = False
     access_key: Optional[str] = None
     secret_key: Optional[str] = None
     endpoint: Optional[str] = None
-    external: bool = False
 
 
 class Volume(BaseAbstraction):
@@ -40,18 +40,34 @@ class Volume(BaseAbstraction):
 
         Parameters:
             name (str):
-                The name of the volume, a descriptive identifier for the data volume.
+                The name of the volume, a descriptive identifier for the data volume. Note that when using an external provider, the name must be the same as the bucket name.
             mount_path (str):
                 The path where the volume is mounted within the container environment.
             config (Optional[VolumeConfig]):
-                Optional configuration for the volume if it is from an external provider.
+                Optional configuration for the volumes from external providers (AWS, Cloudflare, Tigris).
 
         Example:
             ```python
-            from beta9 import Volume
+            from beta9 import Volume, VolumeConfig
 
             # Shared Volume
             shared_volume = Volume(name="model_weights", mount_path="./my-weights")
+
+            # Volume from external provider
+            external_volume = Volume(
+                name="other_model_weights",
+                mount_path="./other-weights",
+                config=VolumeConfig(
+                    external=True,
+                    access_key="my-access-key",
+                    secret_key="my-secret-key",
+                    endpoint="https://s3-endpoint.com",
+                ),
+            )
+
+            @function(volumes=[shared_volume, external_volume])
+            def my_function():
+                pass
             ```
         """
         super().__init__()
