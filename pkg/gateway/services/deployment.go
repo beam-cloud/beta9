4acb7d3e40f17eef8629cd8596077620dd48a317
nickpetrovic <4001122+nickpetrovic@users.noreply.github.com>
2024-08-27 10:43:15 -0400

    Ability to stop scheduled jobs (#486)
    When stopping a deployment, also stop a scheduled job by deleting it.
    
    Match fluent-bit logging settings with production.
    
    Resolve BE-1713

--- pkg/gateway/services/deployment.go
+++ pkg/gateway/services/deployment.go
@@ -153,6 +153,13 @@ func (gws *GatewayService) DeleteDeployment(ctx context.Context, in *pb.DeleteDe
 
 func (gws *GatewayService) stopDeployments(deployments []types.DeploymentWithRelated, ctx context.Context) error {
 	for _, deployment := range deployments {
+		// Stop scheduled job. To re-enable, a new deployment must be created
+		if deployment.StubType == types.StubTypeScheduledJobDeployment {
+			if scheduledJob, err := gws.backendRepo.GetScheduledJob(ctx, deployment.Id); err == nil {
+				gws.backendRepo.DeleteScheduledJob(ctx, scheduledJob)
+			}
+		}
+
 		// Stop active containers
 		containers, err := gws.containerRepo.GetActiveContainersByStubId(deployment.Stub.ExternalId)
 		if err == nil {
