bd5e3ac14a3ffa995e3d37da55b868c4449d9edb
Daniel Levi-Minzi <dleviminzi@gmail.com>
2024-10-23 18:40:29 -0400

    remove slog

--- pkg/worker/lifecycle.go
+++ pkg/worker/lifecycle.go
@@ -6,7 +6,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"log/slog"
 	"os"
 	"path/filepath"
 	"strings"
@@ -230,7 +229,6 @@ func (s *Worker) specFromRequest(request *types.ContainerRequest, options *Conta
 		return nil, err
 	}
 
-	slog.Info("entrypoint", "entrypoint", request.EntryPoint)
 	spec.Process.Cwd = defaultContainerDirectory
 	spec.Process.Args = request.EntryPoint
 
