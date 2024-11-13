845f02df926ff161c1c05bd53713796221849f1a
Nick Petrovic <4001122+nickpetrovic@users.noreply.github.com>
2024-11-13 15:55:16 -0500

    don't use a func

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -2,27 +2,27 @@ package main
 
 import (
 	"log"
-	"os"
 
 	"github.com/beam-cloud/beta9/pkg/types"
 	"github.com/beam-cloud/beta9/pkg/worker"
 )
 
-func handleWorkerError(err error, context string) {
-	if err == nil {
-		return
-	}
-	if _, ok := err.(*types.ErrWorkerNotFound); ok {
-		log.Println("Worker not found. Shutting down.")
-		os.Exit(0)
-	}
-	log.Fatalf("Failed to %s: %v\n", context, err)
-}
-
 func main() {
 	s, err := worker.NewWorker()
-	handleWorkerError(err, "create worker")
+	if err != nil {
+		if _, ok := err.(*types.ErrWorkerNotFound); ok {
+			log.Println("Worker not found. Shutting down.")
+			return
+		}
+		log.Fatal("Worker initialization failed:", err)
+	}
 
 	err = s.Run()
-	handleWorkerError(err, "run worker")
+	if err != nil {
+		if _, ok := err.(*types.ErrWorkerNotFound); ok {
+			log.Println("Worker not found. Shutting down.")
+			return
+		}
+		log.Fatal("Starting worker failed:", err)
+	}
 }
