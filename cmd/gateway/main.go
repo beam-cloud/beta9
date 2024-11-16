c2e12ac3feb1c73ab97b60fd921d8d280726321a
Daniel Levi-Minzi <dleviminzi@gmail.com>
2024-11-16 11:13:51 -0500

    add pretty logging option

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"os"
+
+	"github.com/beam-cloud/beta9/pkg/common"
 	"github.com/beam-cloud/beta9/pkg/gateway"
+	"github.com/beam-cloud/beta9/pkg/types"
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
 func main() {
+	// Initialize logging
+	configManager, err := common.NewConfigManager[types.AppConfig]()
+	if err != nil {
+		log.Fatal().Err(err).Msg("error creating config manager")
+	}
+	config := configManager.GetConfig()
+	if config.PrettyLogs {
+		log.Logger = log.Logger.Level(zerolog.DebugLevel)
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
+		log.Debug().Msg("debug mode enabled")
+	}
 
 	gw, err := gateway.NewGateway()
 	if err != nil {
--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"os"
+
+	"github.com/beam-cloud/beta9/pkg/common"
 	"github.com/beam-cloud/beta9/pkg/types"
 	"github.com/beam-cloud/beta9/pkg/worker"
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
 func main() {
+	configManager, err := common.NewConfigManager[types.AppConfig]()
+	if err != nil {
+		log.Fatal().Err(err).Msg("error creating config manager")
+	}
+	config := configManager.GetConfig()
+	if config.PrettyLogs {
+		log.Logger = log.Logger.Level(zerolog.DebugLevel)
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
+		log.Debug().Msg("debug mode enabled")
+	}
+
 	s, err := worker.NewWorker()
 	if err != nil {
 		if _, ok := err.(*types.ErrWorkerNotFound); ok {
