98490c2c9656b00286909490aacaab1dee333195
luke-lombardi <33990301+luke-lombardi@users.noreply.github.com>
2024-12-05 13:49:28 -0500

    fix migration

--- pkg/repository/backend_postgres_migrations/019_add_workspace_multi_gpu.go
+++ pkg/repository/backend_postgres_migrations/019_add_workspace_multi_gpu.go
@@ -1,21 +1,22 @@
 package backend_postgres_migrations
 
 import (
+	"context"
 	"database/sql"
 
 	"github.com/pressly/goose/v3"
 )
 
 func init() {
-	goose.AddMigration(upAddFieldWorkspaceMultiGpuEnabled, downDropFieldWorkspaceMultiGpuEnabled)
+	goose.AddMigrationContext(upAddFieldWorkspaceMultiGpuEnabled, downDropFieldWorkspaceMultiGpuEnabled)
 }
 
-func upAddFieldWorkspaceMultiGpuEnabled(tx *sql.Tx) error {
+func upAddFieldWorkspaceMultiGpuEnabled(ctx context.Context, tx *sql.Tx) error {
 	_, err := tx.Exec(`ALTER TABLE workspace ADD COLUMN multi_gpu_enabled BOOLEAN DEFAULT FALSE;`)
 	return err
 }
 
-func downDropFieldWorkspaceMultiGpuEnabled(tx *sql.Tx) error {
+func downDropFieldWorkspaceMultiGpuEnabled(ctx context.Context, tx *sql.Tx) error {
 	_, err := tx.Exec(`ALTER TABLE workspace DROP COLUMN multi_gpu_enabled;`)
 	return err
 }
