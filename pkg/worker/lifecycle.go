c3102f4fe0e56a494b7de370bd301dba532bf48f
Daniel Levi-Minzi <dleviminzi@gmail.com>
2024-12-14 12:01:47 -0500

    use worker context for things that should be canceled

--- pkg/worker/lifecycle.go
+++ pkg/worker/lifecycle.go
@@ -226,16 +226,16 @@ func (s *Worker) buildOrPullImage(request *types.ContainerRequest, containerId s
 	switch {
 	case request.BuildOptions.Dockerfile != nil:
 		log.Printf("<%s> - lazy-pull failed, building image from Dockerfile\n", containerId)
-		buildCtxPath, err := getBuildContext(request)
+		buildCtxPath, err := s.getBuildContext(request)
 		if err != nil {
 			return err
 		}
-		if err := s.imageClient.BuildAndArchiveImage(context.TODO(), outputChan, *request.BuildOptions.Dockerfile, request.ImageId, buildCtxPath); err != nil {
+		if err := s.imageClient.BuildAndArchiveImage(s.ctx, outputChan, *request.BuildOptions.Dockerfile, request.ImageId, buildCtxPath); err != nil {
 			return err
 		}
 	case request.BuildOptions.SourceImage != nil:
 		log.Printf("<%s> - lazy-pull failed, pulling source image: %s\n", containerId, *request.BuildOptions.SourceImage)
-		if err := s.imageClient.PullAndArchiveImage(context.TODO(), *request.BuildOptions.SourceImage, request.ImageId, request.BuildOptions.SourceImageCreds); err != nil {
+		if err := s.imageClient.PullAndArchiveImage(s.ctx, *request.BuildOptions.SourceImage, request.ImageId, request.BuildOptions.SourceImageCreds); err != nil {
 			return err
 		}
 	}
@@ -696,10 +696,10 @@ func (s *Worker) watchOOMEvents(ctx context.Context, containerId string, output 
 	}
 }
 
-func getBuildContext(request *types.ContainerRequest) (string, error) {
+func (s *Worker) getBuildContext(request *types.ContainerRequest) (string, error) {
 	buildCtxPath := "."
 	if request.BuildOptions.BuildCtxObject != nil {
-		err := common.ExtractObjectFile(context.TODO(), *request.BuildOptions.BuildCtxObject, request.Workspace.Name)
+		err := common.ExtractObjectFile(s.ctx, *request.BuildOptions.BuildCtxObject, request.Workspace.Name)
 		if err != nil {
 			return "", err
 		}
