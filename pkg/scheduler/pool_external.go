66d7a14aef062a0dac6c4faa05745b72d18da6e9
John <john@slai.io>
2025-01-03 13:55:30 -0700

    cleanup

--- pkg/scheduler/pool_external.go
+++ pkg/scheduler/pool_external.go
@@ -275,8 +275,7 @@ func (wpc *ExternalWorkerPoolController) createWorkerJob(workerId, machineId str
 	workerImage := fmt.Sprintf("%s/%s:%s",
 		wpc.config.Worker.ImageRegistry,
 		wpc.config.Worker.ImageName,
-		// wpc.config.Worker.ImageTag,
-		"devel",
+		wpc.config.Worker.ImageTag,
 	)
 
 	resources := corev1.ResourceRequirements{}
