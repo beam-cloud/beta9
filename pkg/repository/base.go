77dc304288bcdfc0b9982c80c70f22da78d9fd99
Daniel Levi-Minzi <dleviminzi@gmail.com>
2025-01-06 14:14:13 -0500

    send oom event to es

--- pkg/repository/base.go
+++ pkg/repository/base.go
@@ -177,6 +177,7 @@ type EventRepository interface {
 	PushContainerScheduledEvent(containerID string, workerID string, request *types.ContainerRequest)
 	PushContainerStartedEvent(containerID string, workerID string, request *types.ContainerRequest)
 	PushContainerStoppedEvent(containerID string, workerID string, request *types.ContainerRequest)
+	PushContainerOOMEvent(containerID string, workerID string, stubId string)
 	PushContainerResourceMetricsEvent(workerID string, request *types.ContainerRequest, metrics types.EventContainerMetricsData)
 	PushWorkerStartedEvent(workerID string)
 	PushWorkerStoppedEvent(workerID string)
--- pkg/types/event.go
+++ pkg/types/event.go
@@ -47,6 +47,7 @@ var (
 	EventContainerLifecycleScheduled = "scheduled"
 	EventContainerLifecycleStarted   = "started"
 	EventContainerLifecycleStopped   = "stopped"
+	EventContainerLifecycleOOM       = "oom"
 	EventContainerLifecycleFailed    = "failed"
 )
 
