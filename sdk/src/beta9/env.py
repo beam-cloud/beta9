ea1f9ca67302a60a008e94c6b1e729d8995249b5
Daniel Levi-Minzi <dleviminzi@gmail.com>
2025-01-17 18:36:52 -0500

    fix bug in notebook env check

--- sdk/src/beta9/env.py
+++ sdk/src/beta9/env.py
@@ -72,7 +72,7 @@ def try_env(env: str, default: EnvValue) -> Union[EnvValue, bool]:
 
 
 def is_notebook_env() -> bool:
-    if "google.colab" or "marimo" in sys.modules:
+    if "google.colab" in sys.modules or "marimo" in sys.modules:
         return True
 
     try:
