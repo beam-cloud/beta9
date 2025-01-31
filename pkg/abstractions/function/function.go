c381885597f6eaea264643646baed0851af12890
John <john@slai.io>
2025-01-30 23:53:58 -0700

    Revert "Remove function service kill switch from function monitor (#914)"
    This reverts commit 33285349773fb15ca1e8c50287c3f5fde6409a0d.

--- pkg/abstractions/function/function.go
+++ pkg/abstractions/function/function.go
@@ -324,6 +324,9 @@ func (fs *RunCFunctionService) FunctionMonitor(req *pb.FunctionMonitorRequest, s
 
 	for {
 		select {
+		case <-fs.ctx.Done():
+			return nil
+
 		case <-stream.Context().Done():
 			return nil
 
