d2d0f6fa424c0135c86dc4b43bbb59aa7d276622
luke-lombardi <33990301+luke-lombardi@users.noreply.github.com>
2025-01-31 12:42:08 -0500

    add some retry logic

--- pkg/worker/repository.go
+++ pkg/worker/repository.go
@@ -6,13 +6,21 @@ import (
 	"errors"
 	"fmt"
 	"strings"
+	"time"
 
 	common "github.com/beam-cloud/beta9/pkg/common"
 	types "github.com/beam-cloud/beta9/pkg/types"
 	pb "github.com/beam-cloud/beta9/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+)
+
+const (
+	defaultGRPCMaxRetries = 3
+	defaultGRPCRetryDelay = time.Second
 )
 
 // NewWorkerRepositoryClient creates a new worker repository client
@@ -39,21 +47,42 @@ func NewContainerRepositoryClient(ctx context.Context, config types.AppConfig, t
 
 // newGRPCConn creates a new gRPC connection (with or without TLS/Auth) to the provided host
 func newGRPCConn(host string, token string) (*grpc.ClientConn, error) {
-	grpcOption := grpc.WithTransportCredentials(insecure.NewCredentials())
-
-	isTLS := strings.HasSuffix(host, "443")
-	if isTLS {
-		h2creds := credentials.NewTLS(&tls.Config{NextProtos: []string{"h2"}})
-		grpcOption = grpc.WithTransportCredentials(h2creds)
+	creds := insecure.NewCredentials()
+	if strings.HasSuffix(host, "443") {
+		creds = credentials.NewTLS(&tls.Config{NextProtos: []string{"h2"}})
 	}
 
-	var dialOpts = []grpc.DialOption{grpcOption}
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(creds),
+		grpc.WithUnaryInterceptor(retryInterceptor(defaultGRPCMaxRetries, defaultGRPCRetryDelay)),
+	}
 
 	if token != "" {
-		dialOpts = append(dialOpts, grpc.WithUnaryInterceptor(common.ClientAuthInterceptor(token)))
+		opts = append(opts, grpc.WithUnaryInterceptor(common.ClientAuthInterceptor(token)))
 	}
 
-	return grpc.NewClient(host, dialOpts...)
+	return grpc.Dial(host, opts...)
+}
+
+// retryInterceptor retries the call on some gRPC errors
+func retryInterceptor(maxRetries int, delay time.Duration) grpc.UnaryClientInterceptor {
+	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+		for i := 0; i < maxRetries; i++ {
+			err := invoker(ctx, method, req, reply, cc, opts...)
+			if err == nil {
+				return nil
+			}
+
+			st, ok := status.FromError(err)
+			if !ok || (st.Code() != codes.Unavailable && st.Code() != codes.ResourceExhausted) {
+				return err
+			}
+
+			time.Sleep(delay)
+		}
+
+		return errors.New("max retries reached")
+	}
 }
 
 // handleGRPCResponse handles a repository gRPC response and returns the response & an error if the response is not Ok
