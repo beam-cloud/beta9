da4214f7ede2cfad9fc4336106c261ec840dce66
John <john@slai.io>
2025-01-31 16:13:06 -0700

    test new option

--- sdk/src/beta9/channel.py
+++ sdk/src/beta9/channel.py
@@ -53,6 +53,7 @@ class Channel(InterceptorChannel):
 
         options.append(("grpc.keepalive_time_ms", 10000))
         options.append(("grpc.keepalive_timeout_ms", 5000))
+        options.append(("grpc.http2.max_ping_strikes", 1))
         options.append(("grpc.http2.max_pings_without_data", 0))
 
         if credentials is not None:
