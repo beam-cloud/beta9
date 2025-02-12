971f82d98c81c0a12913cf1d040a76efe51278d1
luke-lombardi <33990301+luke-lombardi@users.noreply.github.com>
2025-02-11 21:10:49 -0500

    fix interface

--- pkg/worker/criu_nvidia.go
+++ pkg/worker/criu_nvidia.go
@@ -8,18 +8,20 @@ import (
 )
 
 type NvidiaCRIUManager struct {
+	runcHandle runc.Runc
 }
 
 func InitializeNvidiaCRIU(ctx context.Context, config types.NvidiaCRIUConfig) (CRIUManager, error) {
-	return nil, nil
+	runcHandle := runc.Runc{}
+	return &NvidiaCRIUManager{runcHandle: runcHandle}, nil
 }
 
 func (c *NvidiaCRIUManager) CreateCheckpoint(ctx context.Context, request *types.ContainerRequest) (string, error) {
 	return "", nil
 }
 
-func (c *NvidiaCRIUManager) RestoreCheckpoint(ctx context.Context, request *types.ContainerRequest, state types.CheckpointState, runcOpts *runc.CreateOpts) (chan int, string, error) {
-	return nil, "", nil
+func (c *NvidiaCRIUManager) RestoreCheckpoint(ctx context.Context, request *types.ContainerRequest, state types.CheckpointState, runcOpts *runc.CreateOpts) (chan int, error) {
+	return nil, nil
 }
 
 func (c *NvidiaCRIUManager) Run(ctx context.Context, containerId string, bundle string, gpuEnabled bool, runcOpts *runc.CreateOpts) (chan int, error) {
