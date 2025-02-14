de3377b4b5bab3e795d9f6ad9b284ef10e40184a
luke-lombardi <33990301+luke-lombardi@users.noreply.github.com>
2025-02-13 20:51:12 -0500

    force create hre

--- sdk/src/beta9/abstractions/pod.py
+++ sdk/src/beta9/abstractions/pod.py
@@ -105,7 +105,7 @@ class Pod(RunnerAbstraction):
         self._pod_stub = value
 
     def run(self):
-        if not self.prepare_runtime(stub_type=POD_RUN_STUB_TYPE):
+        if not self.prepare_runtime(stub_type=POD_RUN_STUB_TYPE, force_create_stub=True):
             return False
 
         terminal.header("Running")
