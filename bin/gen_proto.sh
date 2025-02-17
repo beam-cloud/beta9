b8f6913481a2a9098a5efdac5dadccc16832e0e1
Luke Lombardi <luke@Lukes-MacBook-Pro.local>
2025-02-17 12:17:34 -0500

    tidy; clean up proto install

--- bin/gen_proto.sh
+++ bin/gen_proto.sh
@@ -1,59 +1,59 @@
 #!/bin/bash
 
-go2proto -f ./pkg/types/types.proto -p ./pkg/types -n github.com/beam-cloud/beta9/proto -t types
+go2proto -f ../pkg/types/types.proto -p ../pkg/types -n github.com/beam-cloud/beta9/proto -t types
 
 # Generate code for gateway services
-protoc -I ./pkg/types/ --go_out=./proto --go_opt=paths=source_relative --go-grpc_out=./proto --go-grpc_opt=paths=source_relative ./pkg/types/types.proto
-protoc -I ./pkg/types/ --python_betterproto_beta9_out=./sdk/src/beta9/clients/ ./pkg/types/types.proto
+protoc -I ../pkg/types/ --go_out=../proto --go_opt=paths=source_relative --go-grpc_out=../proto --go-grpc_opt=paths=source_relative ../pkg/types/types.proto
+protoc -I ../pkg/types/ --python_betterproto_beta9_out=../sdk/src/beta9/clients/ ../pkg/types/types.proto
 
-protoc -I ./pkg/scheduler/ --go_out=./proto --go_opt=paths=source_relative --go-grpc_out=./proto --go-grpc_opt=paths=source_relative ./pkg/scheduler/scheduler.proto
-protoc -I ./pkg/worker/ --go_out=./proto --go_opt=paths=source_relative --go-grpc_out=./proto --go-grpc_opt=paths=source_relative ./pkg/worker/worker.proto
-protoc -I ./pkg/types -I ./pkg/gateway/ --go_out=./proto --go_opt=paths=source_relative --go-grpc_out=./proto --go-grpc_opt=paths=source_relative ./pkg/gateway/gateway.proto
-protoc -I ./pkg/types -I ./pkg/gateway/ --python_betterproto_beta9_out=./sdk/src/beta9/clients/ ./pkg/gateway/gateway.proto
+protoc -I ../pkg/scheduler/ --go_out=../proto --go_opt=paths=source_relative --go-grpc_out=../proto --go-grpc_opt=paths=source_relative ../pkg/scheduler/scheduler.proto
+protoc -I ../pkg/worker/ --go_out=../proto --go_opt=paths=source_relative --go-grpc_out=../proto --go-grpc_opt=paths=source_relative ../pkg/worker/worker.proto
+protoc -I ../pkg/types -I ../pkg/gateway/ --go_out=../proto --go_opt=paths=source_relative --go-grpc_out=../proto --go-grpc_opt=paths=source_relative ../pkg/gateway/gateway.proto
+protoc -I ../pkg/types -I ../pkg/gateway/ --python_betterproto_beta9_out=../sdk/src/beta9/clients/ ../pkg/gateway/gateway.proto
 
 # Repository services
-protoc -I ./pkg/types -I ./pkg/gateway/services/repository/ --go_out=./proto --go_opt=paths=source_relative --go-grpc_out=./proto --go-grpc_opt=paths=source_relative ./pkg/gateway/services/repository/container_repo.proto
-protoc -I ./pkg/types -I ./pkg/gateway/services/repository/ --go_out=./proto --go_opt=paths=source_relative --go-grpc_out=./proto --go-grpc_opt=paths=source_relative ./pkg/gateway/services/repository/worker_repo.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/types -I ../pkg/gateway/services/repository/ --go_out=../proto --go_opt=paths=source_relative --go-grpc_out=../proto --go-grpc_opt=paths=source_relative ../pkg/gateway/services/repository/container_repo.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/types -I ../pkg/gateway/services/repository/ --go_out=../proto --go_opt=paths=source_relative --go-grpc_out=../proto --go-grpc_opt=paths=source_relative ../pkg/gateway/services/repository/worker_repo.proto
 
 # Generate code for abstractions
-protoc -I ./pkg/abstractions/image/ --go_out=./proto --go_opt=paths=source_relative --go-grpc_out=./proto --go-grpc_opt=paths=source_relative ./pkg/abstractions/image/image.proto
-protoc -I ./pkg/abstractions/image/ --python_betterproto_beta9_out=./sdk/src/beta9/clients/ ./pkg/abstractions/image/image.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/image/ --go_out=../proto --go_opt=paths=source_relative --go-grpc_out=../proto --go-grpc_opt=paths=source_relative ../pkg/abstractions/image/image.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/image/ --python_betterproto_beta9_out=../sdk/src/beta9/clients/ ../pkg/abstractions/image/image.proto
 
-protoc -I ./pkg/abstractions/map/ --go_out=./proto --go_opt=paths=source_relative --go-grpc_out=./proto --go-grpc_opt=paths=source_relative ./pkg/abstractions/map/map.proto
-protoc -I ./pkg/abstractions/map/ --python_betterproto_beta9_out=./sdk/src/beta9/clients/ ./pkg/abstractions/map/map.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/map/ --go_out=../proto --go_opt=paths=source_relative --go-grpc_out=../proto --go-grpc_opt=paths=source_relative ../pkg/abstractions/map/map.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/map/ --python_betterproto_beta9_out=../sdk/src/beta9/clients/ ../pkg/abstractions/map/map.proto
 
-protoc -I ./pkg/abstractions/function/ --go_out=./proto --go_opt=paths=source_relative --go-grpc_out=./proto --go-grpc_opt=paths=source_relative ./pkg/abstractions/function/function.proto
-protoc -I ./pkg/abstractions/function/ --python_betterproto_beta9_out=./sdk/src/beta9/clients/ ./pkg/abstractions/function/function.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/function/ --go_out=../proto --go_opt=paths=source_relative --go-grpc_out=../proto --go-grpc_opt=paths=source_relative ../pkg/abstractions/function/function.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/function/ --python_betterproto_beta9_out=../sdk/src/beta9/clients/ ../pkg/abstractions/function/function.proto
 
-protoc -I ./pkg/abstractions/queue/ --go_out=./proto --go_opt=paths=source_relative --go-grpc_out=./proto --go-grpc_opt=paths=source_relative ./pkg/abstractions/queue/queue.proto
-protoc -I ./pkg/abstractions/queue/ --python_betterproto_beta9_out=./sdk/src/beta9/clients/ ./pkg/abstractions/queue/queue.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/queue/ --go_out=../proto --go_opt=paths=source_relative --go-grpc_out=../proto --go-grpc_opt=paths=source_relative ../pkg/abstractions/queue/queue.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/queue/ --python_betterproto_beta9_out=../sdk/src/beta9/clients/ ../pkg/abstractions/queue/queue.proto
 
-protoc -I ./pkg/abstractions/volume/ --go_out=./proto --go_opt=paths=source_relative --go-grpc_out=./proto --go-grpc_opt=paths=source_relative ./pkg/abstractions/volume/volume.proto
-protoc -I ./pkg/abstractions/volume/ --python_betterproto_beta9_out=./sdk/src/beta9/clients/ ./pkg/abstractions/volume/volume.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/volume/ --go_out=../proto --go_opt=paths=source_relative --go-grpc_out=../proto --go-grpc_opt=paths=source_relative ../pkg/abstractions/volume/volume.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/volume/ --python_betterproto_beta9_out=../sdk/src/beta9/clients/ ../pkg/abstractions/volume/volume.proto
 
-protoc -I ./pkg/abstractions/taskqueue/ --go_out=./proto --go_opt=paths=source_relative --go-grpc_out=./proto --go-grpc_opt=paths=source_relative ./pkg/abstractions/taskqueue/taskqueue.proto
-protoc -I ./pkg/abstractions/taskqueue/ --python_betterproto_beta9_out=./sdk/src/beta9/clients/ ./pkg/abstractions/taskqueue/taskqueue.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/taskqueue/ --go_out=../proto --go_opt=paths=source_relative --go-grpc_out=../proto --go-grpc_opt=paths=source_relative ../pkg/abstractions/taskqueue/taskqueue.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/taskqueue/ --python_betterproto_beta9_out=../sdk/src/beta9/clients/ ../pkg/abstractions/taskqueue/taskqueue.proto
 
-protoc -I ./pkg/abstractions/endpoint/ --go_out=./proto --go_opt=paths=source_relative --go-grpc_out=./proto --go-grpc_opt=paths=source_relative ./pkg/abstractions/endpoint/endpoint.proto
-protoc -I ./pkg/abstractions/endpoint/ --python_betterproto_beta9_out=./sdk/src/beta9/clients/ ./pkg/abstractions/endpoint/endpoint.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/endpoint/ --go_out=../proto --go_opt=paths=source_relative --go-grpc_out=../proto --go-grpc_opt=paths=source_relative ../pkg/abstractions/endpoint/endpoint.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/endpoint/ --python_betterproto_beta9_out=../sdk/src/beta9/clients/ ../pkg/abstractions/endpoint/endpoint.proto
 
-protoc -I ./pkg/abstractions/container/ --go_out=./proto --go_opt=paths=source_relative --go-grpc_out=./proto --go-grpc_opt=paths=source_relative ./pkg/abstractions/container/container.proto
-protoc -I ./pkg/abstractions/container/ --python_betterproto_beta9_out=./sdk/src/beta9/clients/ ./pkg/abstractions/container/container.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/container/ --go_out=../proto --go_opt=paths=source_relative --go-grpc_out=../proto --go-grpc_opt=paths=source_relative ../pkg/abstractions/container/container.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/container/ --python_betterproto_beta9_out=../sdk/src/beta9/clients/ ../pkg/abstractions/container/container.proto
 
-protoc -I ./pkg/abstractions/pod/ --go_out=./proto --go_opt=paths=source_relative --go-grpc_out=./proto --go-grpc_opt=paths=source_relative ./pkg/abstractions/pod/pod.proto
-protoc -I ./pkg/abstractions/pod/ --python_betterproto_beta9_out=./sdk/src/beta9/clients/ ./pkg/abstractions/pod/pod.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/pod/ --go_out=../proto --go_opt=paths=source_relative --go-grpc_out=../proto --go-grpc_opt=paths=source_relative ../pkg/abstractions/pod/pod.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/pod/ --python_betterproto_beta9_out=../sdk/src/beta9/clients/ ../pkg/abstractions/pod/pod.proto
 
-protoc -I ./pkg/abstractions/output/ --go_out=./proto --go_opt=paths=source_relative --go-grpc_out=./proto --go-grpc_opt=paths=source_relative ./pkg/abstractions/output/output.proto
-protoc -I ./pkg/abstractions/output/ --python_betterproto_beta9_out=./sdk/src/beta9/clients/ ./pkg/abstractions/output/output.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/output/ --go_out=../proto --go_opt=paths=source_relative --go-grpc_out=../proto --go-grpc_opt=paths=source_relative ../pkg/abstractions/output/output.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/output/ --python_betterproto_beta9_out=../sdk/src/beta9/clients/ ../pkg/abstractions/output/output.proto
 
-protoc -I ./pkg/abstractions/secret/ --go_out=./proto --go_opt=paths=source_relative --go-grpc_out=./proto --go-grpc_opt=paths=source_relative ./pkg/abstractions/secret/secret.proto
-protoc -I ./pkg/abstractions/secret/ --python_betterproto_beta9_out=./sdk/src/beta9/clients/ ./pkg/abstractions/secret/secret.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/secret/ --go_out=../proto --go_opt=paths=source_relative --go-grpc_out=../proto --go-grpc_opt=paths=source_relative ../pkg/abstractions/secret/secret.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/secret/ --python_betterproto_beta9_out=../sdk/src/beta9/clients/ ../pkg/abstractions/secret/secret.proto
 
-protoc -I ./pkg/abstractions/experimental/signal/ --go_out=./proto --go_opt=paths=source_relative --go-grpc_out=./proto --go-grpc_opt=paths=source_relative ./pkg/abstractions/experimental/signal/signal.proto
-protoc -I ./pkg/abstractions/experimental/signal/ --python_betterproto_beta9_out=./sdk/src/beta9/clients/ ./pkg/abstractions/experimental/signal/signal.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/experimental/signal/ --go_out=../proto --go_opt=paths=source_relative --go-grpc_out=../proto --go-grpc_opt=paths=source_relative ../pkg/abstractions/experimental/signal/signal.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/experimental/signal/ --python_betterproto_beta9_out=../sdk/src/beta9/clients/ ../pkg/abstractions/experimental/signal/signal.proto
 
-protoc -I ./pkg/abstractions/experimental/bot/ --go_out=./proto --go_opt=paths=source_relative --go-grpc_out=./proto --go-grpc_opt=paths=source_relative ./pkg/abstractions/experimental/bot/bot.proto
-protoc -I ./pkg/abstractions/experimental/bot/ --python_betterproto_beta9_out=./sdk/src/beta9/clients/ ./pkg/abstractions/experimental/bot/bot.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/experimental/bot/ --go_out=../proto --go_opt=paths=source_relative --go-grpc_out=../proto --go-grpc_opt=paths=source_relative ../pkg/abstractions/experimental/bot/bot.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/experimental/bot/ --python_betterproto_beta9_out=../sdk/src/beta9/clients/ ../pkg/abstractions/experimental/bot/bot.proto
 
-protoc -I ./pkg/abstractions/shell/ --go_out=./proto --go_opt=paths=source_relative --go-grpc_out=./proto --go-grpc_opt=paths=source_relative ./pkg/abstractions/shell/shell.proto
-protoc -I ./pkg/abstractions/shell/ --python_betterproto_beta9_out=./sdk/src/beta9/clients/ ./pkg/abstractions/shell/shell.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/shell/ --go_out=../proto --go_opt=paths=source_relative --go-grpc_out=../proto --go-grpc_opt=paths=source_relative ../pkg/abstractions/shell/shell.proto
+protoc -I $PROTOC_INCLUDE_PATH -I ../pkg/abstractions/shell/ --python_betterproto_beta9_out=../sdk/src/beta9/clients/ ../pkg/abstractions/shell/shell.proto
--- go.mod
+++ go.mod
@@ -74,9 +74,9 @@ require (
 	go.opentelemetry.io/otel/sdk/metric v1.31.0
 	go.opentelemetry.io/otel/trace v1.33.0
 	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56
-	golang.org/x/net v0.34.0
-	golang.org/x/sync v0.10.0
-	golang.org/x/sys v0.29.0
+	golang.org/x/net v0.35.0
+	golang.org/x/sync v0.11.0
+	golang.org/x/sys v0.30.0
 	golang.org/x/time v0.8.0
 	google.golang.org/grpc v1.68.1
 	google.golang.org/protobuf v1.36.3
@@ -272,12 +272,12 @@ require (
 	go.uber.org/zap v1.27.0 // indirect
 	go4.org/mem v0.0.0-20220726221520-4f986261bf13 // indirect
 	go4.org/netipx v0.0.0-20231129151722-fdeea329fbba // indirect
-	golang.org/x/crypto v0.32.0 // indirect
-	golang.org/x/mod v0.22.0 // indirect
+	golang.org/x/crypto v0.33.0 // indirect
+	golang.org/x/mod v0.23.0 // indirect
 	golang.org/x/oauth2 v0.24.0 // indirect
-	golang.org/x/term v0.28.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
-	golang.org/x/tools v0.29.0 // indirect
+	golang.org/x/term v0.29.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+	golang.org/x/tools v0.30.0 // indirect
 	golang.zx2c4.com/wintun v0.0.0-20230126152724-0fa3db229ce2 // indirect
 	golang.zx2c4.com/wireguard/windows v0.5.3 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20241209162323-e6fa225c2576 // indirect
