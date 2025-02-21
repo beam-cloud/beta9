792483cb5fe40ffc6e47ef7e7375ea75ee1dbcb1
Luke Lombardi <luke@Mac.lan>
2025-02-21 09:46:10 -0500

    remove comment

--- pkg/types/backend.go
+++ pkg/types/backend.go
@@ -253,11 +253,9 @@ type StubConfigV1 struct {
 	Autoscaler         *Autoscaler     `json:"autoscaler"`
 	Extra              json.RawMessage `json:"extra"`
 	CheckpointEnabled  bool            `json:"checkpoint_enabled"`
-
-	// TODO: added these for pod
-	WorkDir    string   `json:"work_dir"`
-	EntryPoint []string `json:"entry_point"`
-	Ports      []uint32 `json:"ports"`
+	WorkDir            string          `json:"work_dir"`
+	EntryPoint         []string        `json:"entry_point"`
+	Ports              []uint32        `json:"ports"`
 }
 
 func (c *StubConfigV1) RequiresGPU() bool {
