9c0c2ad3e2f6857468fc00cf20b884c947b3184f
Luke Lombardi <luke@beam.cloud>
2025-03-05 22:37:44 -0500

    lay out more of it

--- pkg/storage/manager.go
+++ pkg/storage/manager.go
@@ -1,19 +1,43 @@
 package storage
 
+import (
+	"github.com/beam-cloud/beta9/pkg/common"
+	"github.com/beam-cloud/beta9/pkg/types"
+)
+
 type StorageManager struct {
-	storage map[string]Storage
+	storage   *common.SafeMap[Storage]
+	appConfig types.AppConfig
 }
 
-func NewStorageManager() *StorageManager {
+func NewStorageManager(appConfig types.AppConfig) *StorageManager {
 	return &StorageManager{
-		storage: make(map[string]Storage),
+		storage:   common.NewSafeMap[Storage](),
+		appConfig: appConfig,
 	}
 }
 
-func (s *StorageManager) CreateWorkspaceStorage(name string, storage Storage) {
-	s.storage[name] = storage
+func (s *StorageManager) Create(name string, storage Storage) {
+	s.storage.Set(name, storage)
 }
 
-func (s *StorageManager) MountWorkspaceStorage(name string) Storage {
-	return s.storage[name]
+func (s *StorageManager) Mount(name string) Storage {
+	storage, ok := s.storage.Get(name)
+	if !ok {
+		return nil
+	}
+
+	storage.Mount("/mock")
+
+	return storage
+}
+
+func (s *StorageManager) Unmount(name string) {
+	storage, ok := s.storage.Get(name)
+	if !ok {
+		return
+	}
+
+	storage.Unmount("/mock")
+	s.storage.Delete(name)
 }
