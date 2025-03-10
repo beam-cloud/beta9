6ec79c3361fb9363435d687854d296433cb4df5e
Luke Lombardi <luke@beam.cloud>
2025-03-10 13:59:32 -0400

    remove eager cache and consolidate

--- pkg/worker/mount.go
+++ pkg/worker/mount.go
@@ -14,53 +14,39 @@ import (
 )
 
 type ContainerMountManager struct {
-	mountPointPaths    *common.SafeMap[[]string]
-	EagerCacheStubCode bool
+	mountPointPaths *common.SafeMap[[]string]
 }
 
 func NewContainerMountManager(config types.AppConfig) *ContainerMountManager {
 	return &ContainerMountManager{
-		mountPointPaths:    common.NewSafeMap[[]string](),
-		EagerCacheStubCode: config.Worker.EagerCacheStubCode,
+		mountPointPaths: common.NewSafeMap[[]string](),
 	}
 }
 
 // SetupContainerMounts initializes any external storage for a container
 func (c *ContainerMountManager) SetupContainerMounts(request *types.ContainerRequest, outputLogger *slog.Logger) error {
-	// Create local workspace path so we can symlink volumes before the container starts
-	os.MkdirAll(defaultContainerDirectory, os.FileMode(0755))
-
 	for i, m := range request.Mounts {
-		if c.EagerCacheStubCode && m.MountPath == defaultContainerDirectory && !request.Stub.Type.IsServe() {
-			source := m.LocalPath
-			localUserSource := tempUserCodeDir(request.ContainerId)
-			err := copyDirectory(source, localUserSource)
-			if err != nil {
-				log.Error().Str("container_id", request.ContainerId).Err(err).Msg("failed to eagerly copy remote user code to local /mnt/code")
-			} else {
-				request.Mounts[i].LocalPath = localUserSource
-			}
-		} else if m.MountPath == types.WorkerUserCodeVolume && request.Stub.Storage.Id > 0 {
+		if m.MountPath == types.WorkerUserCodeVolume && request.Stub.Storage.Id > 0 {
 			log.Info().Interface("mount", m).Msg("using new storage path")
 
 			objectPath := request.Mounts[i].LocalPath
 			localUserSource := tempUserCodeDir(request.ContainerId)
-			// extractedObjectPath := path.Join(fmt.Sprintf("/workspace/data/%s/unpacked", request.Workspace.Name), request.Stub.Object.ExternalId)
 
 			if _, err := os.Stat(localUserSource); os.IsNotExist(err) {
 				unzipErr := extractZip(objectPath, localUserSource)
 				if unzipErr != nil {
 					log.Error().Str("container_id", request.ContainerId).Err(unzipErr).Msg("failed to unzip object")
+
 					os.RemoveAll(localUserSource)
 				}
 			}
 
 			request.Mounts[i].LocalPath = localUserSource
-
 		}
 
 		if m.MountType == storage.StorageModeMountPoint && m.MountPointConfig != nil {
 			log.Info().Interface("mount", m).Interface("config", m.MountPointConfig).Msg("setting up container mounts")
+
 			// Add containerId to local mount path for mountpoint storage
 			m.LocalPath = path.Join(m.LocalPath, request.ContainerId, m.MountPointConfig.BucketName)
 			request.Mounts[i].LocalPath = m.LocalPath
