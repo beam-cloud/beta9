46bd377e2b6284beb6e3f5fb160fe24c2adbe2c1
Luke Lombardi <luke@beam.cloud>
2025-04-28 21:03:47 -0400

    fix pool health, update migration script

---
+++ pkg/repository/backend_postgres_migrations/026_add_apps_table.go
@@ -0,0 +1,158 @@
+package backend_postgres_migrations
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/pressly/goose/v3"
+)
+
+func init() {
+	goose.AddMigrationContext(upCreateAppTable, downDropAppTable)
+}
+
+type StubForMigration struct {
+	StubID         int64
+	StubName       string
+	DeploymentName string
+	DeploymentID   int64
+	WorkspaceId    uint
+}
+
+const (
+	migrationAppName = "default"
+)
+
+func upCreateAppTable(ctx context.Context, tx *sql.Tx) error {
+	_, err := tx.ExecContext(ctx, `
+		CREATE TABLE app (
+			id SERIAL PRIMARY KEY,
+			external_id UUID DEFAULT uuid_generate_v4() NOT NULL,
+			name VARCHAR(255) NOT NULL,
+			description TEXT NOT NULL,
+			workspace_id INT NOT NULL REFERENCES workspace(id),
+			created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+			updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+			deleted_at TIMESTAMP NULL,
+			UNIQUE (name, workspace_id)
+		);
+	`)
+	if err != nil {
+		return err
+	}
+
+	// update stubs table to add app_id column
+	_, err = tx.ExecContext(ctx, `
+		ALTER TABLE stub
+		ADD COLUMN app_id INT NULL REFERENCES app(id);
+	`)
+	if err != nil {
+		return err
+	}
+
+	// update deployments table to add app_id column
+	_, err = tx.ExecContext(ctx, `
+		ALTER TABLE deployment
+		ADD COLUMN app_id INT NULL REFERENCES app(id);
+	`)
+	if err != nil {
+		return err
+	}
+
+	// create apps based on the deployment name for each existing deployment
+	_, err = tx.ExecContext(ctx, `
+		INSERT INTO app (name, workspace_id, description, created_at, updated_at)
+		SELECT DISTINCT d.name, d.workspace_id, '', NOW(), NOW()
+		FROM deployment d
+		LEFT JOIN app a
+		  ON a.workspace_id = d.workspace_id
+		 AND a.name = d.name
+		WHERE d.app_id IS NULL
+		  AND a.id IS NULL
+		RETURNING id, workspace_id, name;
+	`)
+	if err != nil {
+		return err
+	}
+
+	// update the deployments themselves to reference the new apps
+	_, err = tx.ExecContext(ctx, `
+		UPDATE deployment d
+		SET app_id = a.id
+		FROM app a
+		WHERE d.name = a.name
+		  AND d.workspace_id = a.workspace_id
+		  AND d.app_id IS NULL;
+	`)
+	if err != nil {
+		return err
+	}
+
+	// update the stubs themselves to also reference the new apps
+	_, err = tx.ExecContext(ctx, `
+		UPDATE stub s
+		SET app_id = a.id
+		FROM deployment d
+		JOIN app a ON d.name = a.name AND d.workspace_id = a.workspace_id
+		WHERE d.stub_id = s.id
+		  AND s.app_id IS NULL
+		  AND d.app_id IS NOT NULL;
+	`)
+	if err != nil {
+		return err
+	}
+
+	//  insert "Misc." app for stubs without an app (i.e. serves, functions, shells)
+	_, err = tx.ExecContext(ctx, `
+		INSERT INTO app (name, workspace_id, description, created_at, updated_at)
+		SELECT DISTINCT 'Misc.', s.workspace_id, '', NOW(), NOW()
+		FROM stub s
+		LEFT JOIN app a
+		  ON a.workspace_id = s.workspace_id
+		 AND a.name = 'Misc.'
+		WHERE s.app_id IS NULL
+		  AND a.id IS NULL
+		RETURNING id, workspace_id, name;
+	`)
+	if err != nil {
+		return err
+	}
+
+	// update remaining stubs to reference the "Misc." app
+	_, err = tx.ExecContext(ctx, `
+		UPDATE stub s
+		SET app_id = a.id
+		FROM app a
+		WHERE s.workspace_id = a.workspace_id
+		  AND a.name = 'Misc.'
+		  AND s.app_id IS NULL;
+	`)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func downDropAppTable(ctx context.Context, tx *sql.Tx) error {
+	// Remove app_id column from deployments table
+	_, err := tx.ExecContext(ctx, `
+		ALTER TABLE deployment
+		DROP COLUMN app_id;
+	`)
+	if err != nil {
+		return err
+	}
+
+	// Remove app_id column from stubs table
+	_, err = tx.ExecContext(ctx, `
+		ALTER TABLE stub
+		DROP COLUMN app_id;
+	`)
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.ExecContext(ctx, `DROP TABLE IF EXISTS app;`)
+	return err
+}
--- pkg/scheduler/pool_health.go
+++ pkg/scheduler/pool_health.go
@@ -94,6 +94,7 @@ func (p *PoolHealthMonitor) getPoolState() (*types.WorkerPoolState, error) {
 	runningContainers := 0
 	registeredMachines := 0
 	pendingMachines := 0
+	readyMachines := 0
 
 	workers, err := p.workerRepo.GetAllWorkersInPool(p.wpc.Name())
 	if err != nil {
@@ -111,11 +112,10 @@ func (p *PoolHealthMonitor) getPoolState() (*types.WorkerPoolState, error) {
 			switch machine.State.Status {
 			case types.MachineStatusPending:
 				pendingMachines++
-			// HOTFIX: need to add better checks for whether or not a machine is ready here
-			// case types.MachineStatusRegistered:
-			// 	registeredMachines++
-			case types.MachineStatusReady:
+			case types.MachineStatusRegistered:
 				registeredMachines++
+			case types.MachineStatusReady:
+				readyMachines++
 			}
 		}
 	}
@@ -187,6 +187,7 @@ func (p *PoolHealthMonitor) getPoolState() (*types.WorkerPoolState, error) {
 		FreeMemory:         freeCapacity.FreeMemory,
 		RegisteredMachines: int64(registeredMachines),
 		PendingMachines:    int64(pendingMachines),
+		ReadyMachines:      int64(readyMachines),
 	}, nil
 }
 
@@ -206,9 +207,9 @@ func (p *PoolHealthMonitor) updatePoolStatus(nextState *types.WorkerPoolState) e
 		failoverReasons = append(failoverReasons, "exceeded max scheduling latency")
 	}
 
-	if (nextState.RegisteredMachines < p.workerConfig.Failover.MinMachinesAvailable) && p.wpc.Mode() == types.PoolModeExternal {
+	if (nextState.ReadyMachines < p.workerConfig.Failover.MinMachinesAvailable) && p.wpc.Mode() == types.PoolModeExternal {
 		status = types.WorkerPoolStatusDegraded
-		failoverReasons = append(failoverReasons, "not enough registered machines")
+		failoverReasons = append(failoverReasons, "not enough ready machines")
 	}
 
 	nextState.Status = status
