41a1fd61a7693bc8fa4f3e9cd2c0b209f002e7a7
Daniel Levi-Minzi <dleviminzi@gmail.com>
2025-05-27 10:08:24 -0400

    remove backfill

--- pkg/repository/backend_postgres_migrations/029_extend_image_table.go
+++ pkg/repository/backend_postgres_migrations/029_extend_image_table.go
@@ -17,16 +17,7 @@ func upExtendImageTable(ctx context.Context, tx *sql.Tx) error {
 		ADD COLUMN external_id UUID DEFAULT uuid_generate_v4() UNIQUE NOT NULL,
 		ADD COLUMN created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
 		ADD COLUMN updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP;
-	`)
-	if err != nil {
-		return err
-	}
-
-	_, err = tx.ExecContext(ctx, `
-		UPDATE image 
-		SET created_at = CURRENT_TIMESTAMP, updated_at = CURRENT_TIMESTAMP 
-		WHERE created_at IS NULL OR updated_at IS NULL;
-	`)
+		`)
 	return err
 }
 
