ac211a4141a4fc9ddae2f6c149609b8b54383961
Daniel Levi-Minzi <51272568+dleviminzi@users.noreply.github.com>
2025-06-02 11:47:51 -0400

    Update sdk/src/beta9/abstractions/integrations/vllm.py

--- sdk/src/beta9/abstractions/integrations/vllm.py
+++ sdk/src/beta9/abstractions/integrations/vllm.py
@@ -168,7 +168,7 @@ class VLLM(ASGI):
             The container image used for the task execution. Whatever you pass here will have an additional `add_python_packages` call
             with `["fastapi", "vllm", "huggingface_hub"]` added to it to ensure that we can run vLLM in the container.
         vllm_version (str):
-            The version of vLLM that will be installed from PyPI. As the configuration of the vLLM engine depend on the version of vLLM, using a non-default vllm_version might require subclassing VLLMArgs in order to add the missing configuration options. Default is version 0.8.4.
+            The version of vLLM that will be installed from PyPI. As the configuration of the vLLM engine depends on the version of vLLM, using a non-default vllm_version might require subclassing VLLMArgs in order to add the missing configuration options. Default is version 0.8.4.
         huggingface_hub_version (str):
             The version of huggingface_hub that will be installed from PyPI. Different versions of vLLM require different versions of huggingface_hub, thus using a non-default vLLM version might require using a non-default version of huggingface_hub.  Default is version 0.30.2.
         workers (int):
