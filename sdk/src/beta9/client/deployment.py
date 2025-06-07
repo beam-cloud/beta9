8f7695a981e1ee6b7ffed147c9bcc4516e500a16
Luke Lombardi <luke@beam.cloud>
2025-06-07 12:49:23 -0700

    cleanup

--- sdk/src/beta9/client/deployment.py
+++ sdk/src/beta9/client/deployment.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Union
+from typing import Any, Callable, Optional, Union
 
 from ..exceptions import DeploymentNotFoundError, TaskNotFoundError
 from . import get_deployment_url, get_stub_url, post
@@ -7,21 +7,36 @@ from .task import Task
 
 class Deployment:
     def __init__(
-        self, *, base_url: str, deployment_id: str, stub_id: str, token: str, workspace_id: str
+        self,
+        *,
+        base_url: str,
+        token: str,
+        workspace_id: str,
+        stub_id: Optional[str] = None,
+        deployment_id: Optional[str] = None,
+        deployment_url: Optional[str] = None,
     ):
-        self.id = deployment_id
         self.stub_id = stub_id
+        self.deployment_id = deployment_id
         self.token = token
         self.base_url = base_url
         self.workspace_id = workspace_id
 
-        if self.stub_id:
+        if deployment_url:
+            self.url = deployment_url
+        elif self.stub_id:
             self.url = get_stub_url(token=self.token, url=self.base_url, id=self.stub_id)
+        elif self.deployment_id:
+            self.url = get_deployment_url(
+                token=self.token, url=self.base_url, id=self.deployment_id
+            )
         else:
-            self.url = get_deployment_url(token=self.token, url=self.base_url, id=self.id)
+            raise ValueError(
+                "Deployment() requires either a deployment ID, stub ID, or deployment URL"
+            )
 
     def submit(self, *, input: dict = {}) -> Union[Task, Any]:
-        """Submit a task to the deployment. Returns a Task object if the task runs asynchronously, otherwise blocks until the task is complete and returns the JSON response."""
+        """Submit a task to the deployment. Returns a Task object if the task runs asynchronously, otherwise blocks until the task is complete and returns a JSON response."""
 
         if not self.url:
             raise TaskNotFoundError(f"Failed to get retrieve URL for task {self.id}")
@@ -38,7 +53,7 @@ class Deployment:
         return body
 
     def subscribe(self, *, input: dict = {}, event_handler: Callable = None) -> Any:
-        """Submit a task to the deployment, and subscribe to the task. Yields updates as task status changes. Returns an iterable of JSON objects."""
+        """Submit a task to the deployment, and subscribe to the task. Yields updates as task status changes. Returns the JSON response from the deployment, or None."""
 
         if not self.url:
             raise DeploymentNotFoundError(f"Failed to get retrieve URL for task {id}")
