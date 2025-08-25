b5970b0d401e8c16a683eef382c4202496a9f3ff
Cooper <cooper@beam.cloud>
2025-08-25 13:48:59 -0700

    Added container path to download file method

--- proto/pod.pb.gw.go
+++ proto/pod.pb.gw.go
@@ -395,8 +395,6 @@ func local_request_PodService_SandboxUploadFile_0(ctx context.Context, marshaler
 	return msg, metadata, err
 }
 
-var filter_PodService_SandboxDownloadFile_0 = &utilities.DoubleArray{Encoding: map[string]int{"container_id": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
-
 func request_PodService_SandboxDownloadFile_0(ctx context.Context, marshaler runtime.Marshaler, client PodServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var (
 		protoReq PodSandboxDownloadFileRequest
@@ -414,11 +412,13 @@ func request_PodService_SandboxDownloadFile_0(ctx context.Context, marshaler run
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "container_id", err)
 	}
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	val, ok = pathParams["container_path"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "container_path")
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_PodService_SandboxDownloadFile_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	protoReq.ContainerPath, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "container_path", err)
 	}
 	msg, err := client.SandboxDownloadFile(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
@@ -438,11 +438,13 @@ func local_request_PodService_SandboxDownloadFile_0(ctx context.Context, marshal
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "container_id", err)
 	}
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	val, ok = pathParams["container_path"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "container_path")
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_PodService_SandboxDownloadFile_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	protoReq.ContainerPath, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "container_path", err)
 	}
 	msg, err := server.SandboxDownloadFile(ctx, &protoReq)
 	return msg, metadata, err
@@ -1151,7 +1153,7 @@ func RegisterPodServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux,
 		var stream runtime.ServerTransportStream
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		annotatedContext, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/pod.PodService/SandboxDownloadFile", runtime.WithHTTPPathPattern("/pods/{container_id}/files/download"))
+		annotatedContext, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/pod.PodService/SandboxDownloadFile", runtime.WithHTTPPathPattern("/pods/{container_id}/files/download/{container_path=**}"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -1565,7 +1567,7 @@ func RegisterPodServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux,
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		annotatedContext, err := runtime.AnnotateContext(ctx, mux, req, "/pod.PodService/SandboxDownloadFile", runtime.WithHTTPPathPattern("/pods/{container_id}/files/download"))
+		annotatedContext, err := runtime.AnnotateContext(ctx, mux, req, "/pod.PodService/SandboxDownloadFile", runtime.WithHTTPPathPattern("/pods/{container_id}/files/download/{container_path=**}"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -1777,7 +1779,7 @@ var (
 	pattern_PodService_SandboxKill_0            = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1, 2, 2}, []string{"pods", "container_id", "kill"}, ""))
 	pattern_PodService_SandboxListProcesses_0   = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1, 2, 2}, []string{"pods", "container_id", "processes"}, ""))
 	pattern_PodService_SandboxUploadFile_0      = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1, 2, 2, 2, 3}, []string{"pods", "container_id", "files", "upload"}, ""))
-	pattern_PodService_SandboxDownloadFile_0    = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1, 2, 2, 2, 3}, []string{"pods", "container_id", "files", "download"}, ""))
+	pattern_PodService_SandboxDownloadFile_0    = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1, 2, 2, 2, 3, 3, 0, 4, 1, 5, 4}, []string{"pods", "container_id", "files", "download", "container_path"}, ""))
 	pattern_PodService_SandboxStatFile_0        = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1, 2, 2, 2, 3}, []string{"pods", "container_id", "files", "stat"}, ""))
 	pattern_PodService_SandboxListFiles_0       = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1, 2, 2}, []string{"pods", "container_id", "files"}, ""))
 	pattern_PodService_SandboxDeleteFile_0      = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1, 2, 2, 3, 0, 4, 1, 5, 3}, []string{"pods", "container_id", "files", "container_path"}, ""))
