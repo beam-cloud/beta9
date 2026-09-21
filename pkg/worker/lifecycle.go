cfda7b9ffa88d7097f583d657cf8074d322f0d06
Luke Lombardi <33990301+luke-lombardi@users.noreply.github.com>
2026-09-21 13:28:38 -0400

    wip

--- pkg/worker/lifecycle.go
+++ pkg/worker/lifecycle.go
@@ -1911,7 +1911,7 @@ func (s *Worker) spawn(request *types.ContainerRequest, spec *specs.Spec, output
 					"success": fmt.Sprintf("%t", err == nil),
 				})
 				if err != nil && ctx.Err() == nil {
-					log.Error().Err(err).Str("container_id", containerId).Msg("failed to expose sandbox memory and cpu limits; workload will see host resources")
+					log.Error().Err(err).Str("container_id", containerId).Msg("failed to expose sandbox memory and cpu limits")
 				}
 			}
 			instance.SandboxProcessManager = processManagerClient
